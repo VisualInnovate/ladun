01586076b5b89f4d6cb2e093104916dec20ccd73
AmrSaleh2234 <amrsaleh1001@gmail.com>
2023-02-27 15:15:28 +0200

    first publish

---
+++ app/Filament/Resources/ProjectResource.php
@@ -0,0 +1,236 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ProjectResource\Pages;
+use App\Filament\Resources\ProjectResource\RelationManagers;
+use App\Models\Project;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Closure;
+use Filament\Forms\Components\Card;
+use Filament\Forms\Components\MultiSelect;
+use Illuminate\Support\Str;
+use Filament\Forms\Components\SpatieMediaLibraryFileUpload;
+use Filament\Tables\Columns\SpatieMediaLibraryImageColumn;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
+use Filament\Forms\Components\FileUpload;
+use Livewire\TemporaryUploadedFile;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Resources\Concerns\Translatable;
+use Filament\Forms\Components\Fieldset;
+use Filament\Forms\Components\CheckboxList;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Repeater;
+
+class ProjectResource extends Resource
+{
+    use Translatable;
+
+    protected static ?string $model = Project::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-office-building';
+    protected static ?int $NavigationSort = 1;
+
+
+    public static function form(Form $form): Form
+    {
+        return $form
+        ->schema([
+            Fieldset::make('Add Project')
+
+            ->schema([
+                TextInput::make('name')->label(__('name'))->reactive()
+                ->afterStateUpdated(function (Closure $set, $state) {
+                    $set('slug', Str::slug($state));
+                })->required(),
+            TextInput::make('slug')->label(__('slug'))->required(),
+            TextInput::make('phone')->label(__('phone'))
+            ->tel()
+            ->telRegex('/^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\.\/0-9]*$/')
+            ->required(),
+            TextInput::make('email')->label(__('email'))
+            ->email(),
+            TextInput::make('address')->label(__('address'))
+            ->required(),
+
+                    SpatieMediaLibraryFileUpload::make('Main_image')->label(__('Main_image'))->collection('projects'),
+                    SpatieMediaLibraryFileUpload::make('attachments')->label(__('attachments'))
+            ->multiple()
+            ->enableReordering(),
+            FileUpload::make('attachment')->label(__('attachment'))->acceptedFileTypes(['application/pdf'])
+            ->getUploadedFileNameForStorageUsing(function (TemporaryUploadedFile $file): string {
+                return (string) str($file->getClientOriginalName())->prepend('custom-prefix-');
+            })->directory('form-attachments')->multiple()
+            ->enableDownload(),
+            TextInput::make('Land_area')->label(__('Land_area'))->required(),
+            TextInput::make('building_area')->label(__('building_area'))
+            ->required()
+            ->numeric(),
+            TextInput::make('units_number')->label(__('units_number'))
+            ->required()
+            ->numeric(),
+            TextInput::make('models_number')->label(__('models_number'))
+            ->required()
+            ->numeric(),
+            CheckboxList::make('utilities')->label(__('utilities'))
+                ->relationship('utilities', 'title')->columns(3),
+                        RichEditor::make('content')->label(__('content'))->columns(2),
+
+
+            Toggle::make('is_published')->label(trans('is_published')),
+
+
+            ])
+            ->columns(3),
+
+            //===========================
+            // Downloads field for peoject
+            //=======================
+
+            Section::make('Downloads  ')
+
+            ->schema([
+                Repeater::make('members')
+                ->relationship('downloads')
+                ->schema([
+                    TextInput::make('name')->required(),
+                    FileUpload::make('project_attachment')->acceptedFileTypes(['application/pdf'])
+->getUploadedFileNameForStorageUsing(function (TemporaryUploadedFile $file): string {
+    return (string) str($file->getClientOriginalName())->prepend('custom-prefix-');
+})->directory('Download-attachments'),
+                ])
+                ->columns(2)
+
+
+            ])
+            ->collapsed(),
+
+
+    //=====================
+    // Models of Projects
+    //======================
+    Section::make('Models ')
+    ->schema([
+        Repeater::make('Models')
+         ->relationship('project_models')
+        ->schema([
+            TextInput::make('title')->required(),
+            TextInput::make('Land_area')->required(),
+            TextInput::make('building_area')->required(),
+            TextInput::make('units_number')->required(),
+            TextInput::make('floors_number')->required(),
+            CheckboxList::make('utilities')->label(__('utilities'))
+            ->relationship('utilities', 'title'),
+        ])
+        ->columns(2)
+
+
+    ])
+    ->collapsed(),
+
+
+    //=====================
+    // Locations for  Projects
+    //======================
+
+
+    Section::make('Locations')
+    ->relationship('location')
+    ->description('Description')
+    ->schema([
+        TextInput::make('city')->required(),
+            TextInput::make('address')->required(),
+            TextInput::make('Latitude')->required(),
+            TextInput::make('Longitude')->required()
+    ])
+
+    ->collapsed(),
+
+
+
+
+        ]);
+}
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('id')->label(__('id'))->sortable(),
+                Tables\Columns\TextColumn::make('name')->label(__('name'))->limit('50')->sortable(),
+                Tables\Columns\TextColumn::make('slug')->label(__('slug'))->limit('50'),
+                IconColumn::make('is_published')->label(trans('is_published'))->boolean(),
+                SpatieMediaLibraryImageColumn::make('Main_image')->label(__('Main_image'))->collection('projects'),
+
+
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListProjects::route('/'),
+            'create' => Pages\CreateProject::route('/create'),
+            'edit' => Pages\EditProject::route('/{record}/edit'),
+        ];
+    }
+
+
+    public static function getTranslatableLocales(): array
+    {
+        return ['en', 'ar'];
+    }
+
+
+    protected static function getNavigationGroup(): ?string
+    {
+        return __('Projects');
+    }
+
+    public static function getLabel(): ?string
+    {
+        return __('Project');
+    }
+
+    public static function getPluralLabel(): ?string
+    {
+        return __('Project');
+    }
+
+    protected static function getNavigationBadge(): ?string
+    {
+        return self::getModel()::count();
+    }
+
+
+
+
+
+
+
+}
