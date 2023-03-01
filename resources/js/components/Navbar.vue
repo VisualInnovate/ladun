e9df2ab32a307f53c0fc57fb6f07f8678b7b1dfa
Ali Ragab <ali@visualinnovate.com>
2023-03-01 14:27:11 +0200

    updated nabar component

---
+++ resources/js/components/Navbar.vue
@@ -0,0 +1,91 @@
+<template>
+    <nav
+        class="z-30 flex items-center justify-between relative"
+        aria-label="Global"
+    >
+        <div class="flex lg:flex-1">
+            <a href="#" class="-m-1.5 p-1.5">
+                <span class="sr-only">Ladun</span>
+                <img
+                    v-show="!mobileMenuOpen"
+                    src="../../img/logo.svg"
+                    alt=""
+                />
+            </a>
+        </div>
+        <div class="flex lg:hidden">
+            <button
+                type="button"
+                class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+                @click="mobileMenuOpen = true"
+            >
+                <span class="sr-only">Open main menu</span>
+                <Bars3Icon class="h-6 w-6" aria-hidden="true" />
+            </button>
+        </div>
+        <div class="hidden lg:flex lg:gap-x-12 ltr:right-0 rtl:left-0">
+            <a
+                v-for="item in navigation"
+                :key="item.name"
+                :href="item.href"
+                class="text-md font-light leading-6 text-white"
+                >{{ item.name }}</a
+            >
+        </div>
+    </nav>
+    <Dialog as="div" @close="mobileMenuOpen = false" :open="mobileMenuOpen">
+        <DialogPanel
+            class="fixed inset-0 z-10 overflow-y-auto px-6 py-6 lg:hidden"
+        >
+            <div class="flex items-center justify-between">
+                <a href="#" class="-m-1.5 p-1.5">
+                    <span class="sr-only">Ladun</span>
+                    <img
+                        aria-hidden="true"
+                        class="h-10"
+                        src="../../img/logo.svg"
+                        alt=""
+                    />
+                </a>
+                <button
+                    type="button"
+                    class="-m-2.5 rounded-md p-2.5 text-gray-700"
+                    @click="mobileMenuOpen = false"
+                >
+                    <span class="sr-only">Close menu</span>
+                    <XMarkIcon class="h-6 w-6" aria-hidden="true" />
+                </button>
+            </div>
+            <div class="mt-6 flow-root">
+                <div class="-my-6 divide-y divide-gray-500/10">
+                    <div class="space-y-2 py-6">
+                        <a
+                            v-for="item in navigation"
+                            :key="item.name"
+                            :href="item.href"
+                            class="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-400/10"
+                            >{{ item.name }}</a
+                        >
+                    </div>
+                </div>
+            </div>
+        </DialogPanel>
+    </Dialog>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { Dialog, DialogPanel } from "@headlessui/vue";
+import { Bars3Icon, XMarkIcon } from "@heroicons/vue/24/outline";
+
+const navigation = [
+    { name: "Main", href: "#" },
+    { name: "About us", href: "#" },
+    { name: "Companies", href: "#" },
+    { name: "Projects", href: "#" },
+    { name: "Investors Relations", href: "#" },
+    { name: "Contact us", href: "#" },
+    { name: "Join us", href: "#" },
+];
+const mobileMenuOpen = ref(false);
+</script>
