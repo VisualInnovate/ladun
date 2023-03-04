12769bdd753968384ca5f70c65e33dbcab2622c2
Ali Ragab <ali@visualinnovate.com>
2023-03-04 19:09:06 +0200

    set default page dir to ltr

--- resources/js/plugins/i18n/index.js
+++ resources/js/plugins/i18n/index.js
@@ -2,6 +2,7 @@ import { createI18n } from "vue-i18n";
 import en from '../../locales/en'
 import ar from '../../locales/ar'
 
+document.body.dir = 'ltr'
 export default new createI18n({
     legacy: false, // Vuetify does not support the legacy mode of vue-i18n
     globalInjection: true,
