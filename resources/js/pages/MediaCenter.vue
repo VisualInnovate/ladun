bbc54555596fd7bcf8a18f8124f8a9b700ee9d6d
Ali Ragab <ali@visualinnovate.com>
2023-03-06 19:49:01 +0200

    added new carousel from flowbite-vue

--- resources/js/pages/MediaCenter.vue
+++ resources/js/pages/MediaCenter.vue
@@ -1,124 +1,116 @@
 <template>
 
-    <div id="default-carousel" class="relative" data-carousel="static">
-        <!-- Carousel wrapper -->
-        <div class="relative h-screen overflow-hidden rounded-lg">
-            <!-- Item 1 -->
-            <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                <span class="absolute text-2xl font-semibold text-white -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 sm:text-3xl dark:text-gray-800">First Slide</span>
-                <img src="../../img/banner-mediaCenter.png" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="media-center">
-            </div>
-            <!-- Item 2 -->
-            <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                <img src="../../img/banner-mediaCenter.png" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="media-center">
-            </div>
-            <!-- Item 3 -->
-            <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                <img src="../../img/banner-mediaCenter.png" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="media-center">
-            </div>
-        </div>
-        <!-- Slider indicators -->
-        <div class="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-14 left-1/2">
-            <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 1" data-carousel-slide-to="0"></button>
-            <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
-            <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
-        </div>
-        <!-- Slider controls -->
-        <button type="button" class="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
-        <span class="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg aria-hidden="true" class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path></svg>
-            <span class="sr-only">Previous</span>
-        </span>
-        </button>
-        <button type="button" class="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
-        <span class="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg aria-hidden="true" class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path></svg>
-            <span class="sr-only">Next</span>
-        </span>
-        </button>
-    </div>
-
-
+    <Carousel :pictures="pictures"></Carousel>
     <section class="media-center py-28 bg-background-section">
         <div class="container mx-auto">
-            <h1 class="text-3xl text-center mb-7">
-                المركز الاعلامي
-            </h1>
-            <div class="grid grid-cols-1 gap-12  lg:grid-cols-2 lg:gap-10">
-
-                <card-link >
-                    <template #date>الخميس 15 مايو 2022</template>
+            <h1 class="text-3xl text-center mb-7">المركز الاعلامي</h1>
+            <div class="grid grid-cols-1 gap-12 lg:grid-cols-2 lg:gap-10">
+                <card-link>
+                    <template #date>السبت 15 مايو 2022</template>
                     <template #head>ملتقى التطوع البلدي</template>
-                    <template #text> لدن للاستثمار الراعي والشريك الأساسي في ملتقى التطوع البلدي الأول في مكة المكرمة</template>
+                    <template #text>
+                        لدن للاستثمار الراعي والشريك الأساسي في ملتقى التطوع
+                        البلدي الأول في مكة المكرمة</template
+                    >
                     <img
                         class="h-full w-full object-cover img-media-center rounded-t-lg md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
-                        src="../../img/media-center.png" alt="">
+                        src="../../img/media-center.png"
+                        alt=""
+                    />
                 </card-link>
 
-                <card-link >
+                <card-link>
                     <template #date>الخميس 15 مايو 2022</template>
                     <template #head>ملتقى التطوع البلدي</template>
-                    <template #text> لدن للاستثمار الراعي والشريك الأساسي في ملتقى التطوع البلدي الأول في مكة المكرمة</template>
+                    <template #text>
+                        لدن للاستثمار الراعي والشريك الأساسي في ملتقى التطوع
+                        البلدي الأول في مكة المكرمة</template
+                    >
                     <img
                         class="h-full object-cover img-media-center rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
-                        src="../../img/media-center.png" alt="">
+                        src="../../img/media-center.png"
+                        alt=""
+                    />
                 </card-link>
 
-
-                <card-link >
+                <card-link>
                     <template #date>الخميس 15 مايو 2022</template>
                     <template #head>ملتقى التطوع البلدي</template>
-                    <template #text> لدن للاستثمار الراعي والشريك الأساسي في ملتقى التطوع البلدي الأول في مكة المكرمة</template>
+                    <template #text>
+                        لدن للاستثمار الراعي والشريك الأساسي في ملتقى التطوع
+                        البلدي الأول في مكة المكرمة</template
+                    >
                     <img
                         class="h-full object-cover img-media-center rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
-                        src="../../img/media-center.png" alt="">
+                        src="../../img/media-center.png"
+                        alt=""
+                    />
                 </card-link>
 
-
-                <card-link >
+                <card-link>
                     <template #date>الخميس 15 مايو 2022</template>
                     <template #head>ملتقى التطوع البلدي</template>
-                    <template #text> لدن للاستثمار الراعي والشريك الأساسي في ملتقى التطوع البلدي الأول في مكة المكرمة</template>
+                    <template #text>
+                        لدن للاستثمار الراعي والشريك الأساسي في ملتقى التطوع
+                        البلدي الأول في مكة المكرمة</template
+                    >
                     <img
                         class="h-full object-cover img-media-center rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
-                        src="../../img/media-center.png" alt="">
+                        src="../../img/media-center.png"
+                        alt=""
+                    />
                 </card-link>
 
-
-                <card-link >
+                <card-link>
                     <template #date>الخميس 15 مايو 2022</template>
                     <template #head>ملتقى التطوع البلدي</template>
-                    <template #text> لدن للاستثمار الراعي والشريك الأساسي في ملتقى التطوع البلدي الأول في مكة المكرمة</template>
+                    <template #text>
+                        لدن للاستثمار الراعي والشريك الأساسي في ملتقى التطوع
+                        البلدي الأول في مكة المكرمة</template
+                    >
                     <img
                         class="h-full object-cover img-media-center rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
-                        src="../../img/media-center.png" alt="">
+                        src="../../img/media-center.png"
+                        alt=""
+                    />
                 </card-link>
-
-
-
-
-
             </div>
         </div>
-
     </section>
 
 
 </template>
 
-<script>
+<script setup>
 import CardLink from "@/components/CardLink.vue";
+import { Carousel } from "flowbite-vue";
 
-export default {
-    components:{
-        CardLink
-    }
-}
+const projects_1 = new URL('../../img/projects_1.png', import.meta.url).href
+const projects_2 = new URL('../../img/projects_2.png', import.meta.url).href
+const projects_3 = new URL('../../img/projects_3.png', import.meta.url).href
+const projects_4 = new URL('../../img/projects_3.png', import.meta.url).href
+const pictures = [
+    {
+        src: projects_1,
+        alt: 'Picture 1',
+    },
+    {
+        src: projects_4,
+        alt: 'Picture 4',
+    },
+    {
+        src: projects_2,
+        alt: 'Picture 2',
+    },
+    {
+        src: projects_3,
+        alt: 'Picture 3',
+    },
+];
 </script>
 
 <style>
 .img-media-center {
-
     width: 250px;
 }
 </style>
