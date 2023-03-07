2c6c0cef5c0357a57942ccb1e001a30404a572e5
Ali Ragab <ali@visualinnovate.com>
2023-03-07 10:58:08 +0200

    added favicon link

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,12 +4,11 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>main page </title>
-<link rel="preconnect" href="https://fonts.googleapis.com">
+	<title>Ladun</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;500;600&display=swap" rel="stylesheet">
-
-
+    <link rel="icon" type="image/x-icon" href="/favicon.ico">
     @vite(['resources/css/app.css','resources/js/main.js'])
 
 </head>
