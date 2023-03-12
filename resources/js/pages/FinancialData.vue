4caac466f94b00afc69a409052ae219c2056a3d5
AmrSaleh2234 <amrsaleh1001@gmail.com>
2023-03-12 13:49:47 +0200

    financials page

--- resources/js/pages/FinancialData.vue
+++ resources/js/pages/FinancialData.vue
@@ -1,5 +1,5 @@
 <template>
-    <Navbar class="bg-black fixed z-50 w-full" />
+    <Navbar class="bg-black fixed z-50 w-full"/>
     <img-banner>
         <img src="../../img/finncial-banner.png">
         <template #text> البيانات الماليه</template>
@@ -8,17 +8,158 @@
 
     <section class="media-center py-28 bg-background-section">
         <div class="container mx-auto">
-            <header class="flex flex-wrap justify-between items-center">
-                <div
-                    class="bg-subtraction-ltr-png rtl:bg-subtraction-png h-16 w-[80%] flex items-center rtl:pr-8  ltr:pl-8 text-xl">
-                    البيانات المالية الخاصة لعام
+            <div class="mb-12">
+
+                <header class="flex flex-wrap justify-between items-center mb-5">
+                    <div
+                        class="bg-subtraction-ltr-png rtl:bg-subtraction-png h-16 w-[80%] flex items-center rtl:pr-8  ltr:pl-8 text-xl">
+                        البيانات المالية الخاصة لعام
+                    </div>
+                    <div class="year flex items-center text-[50px] font-bold " dir="rtl">
+                        <span class="text-dark-brown">23</span><span>20</span>
+                    </div>
+                </header>
+
+                <div class="card w-[80%] flex">
+
+                    <div
+                        class=" mx-4 w-60 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-10">
+                        <div class="flex flex-col items-center ">
+                            <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="../../img/company.png"
+                                 alt="Bonnie image"/>
+                            <h5 class="mb-1 text-center text-xl font-medium text-gray-900 dark:text-white">Bonnie
+                                Green</h5>
+                            <span class="text-sm text-gray-500 dark:text-gray-400">Visual Designer</span>
+                            <div class="flex  space-x-3 text-dark-brown font-bold">
+                                2023
+                            </div>
+                        </div>
+                    </div>
+
+                    <div
+                        class=" mx-4 w-60 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-10">
+                        <div class="flex flex-col items-center ">
+                            <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="../../img/company.png"
+                                 alt="Bonnie image"/>
+                            <h5 class="mb-1 text-center text-xl font-medium text-gray-900 dark:text-white">Bonnie
+                                Green</h5>
+                            <span class="text-sm text-gray-500 dark:text-gray-400">Visual Designer</span>
+                            <div class="flex  space-x-3 text-dark-brown font-bold">
+                                2023
+                            </div>
+                        </div>
+                    </div>
+
+                    <div
+                        class=" mx-4 w-60 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-10">
+                        <div class="flex flex-col items-center ">
+                            <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="../../img/company.png"
+                                 alt="Bonnie image"/>
+                            <h5 class="mb-1 text-center text-xl font-medium text-gray-900 dark:text-white">Bonnie
+                                Green</h5>
+                            <span class="text-sm text-gray-500 dark:text-gray-400">Visual Designer</span>
+                            <div class="flex  space-x-3 text-dark-brown font-bold">
+                                2023
+                            </div>
+                        </div>
+                    </div>
+
+                    <div
+                        class=" mx-4 w-60 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-10">
+                        <div class="flex flex-col items-center ">
+                            <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="../../img/company.png"
+                                 alt="Bonnie image"/>
+                            <h5 class="mb-1 text-center text-xl font-medium text-gray-900 dark:text-white">Bonnie
+                                Green</h5>
+                            <span class="text-sm text-gray-500 dark:text-gray-400">Visual Designer</span>
+                            <div class="flex  space-x-3 text-dark-brown font-bold">
+                                2023
+                            </div>
+                        </div>
+                    </div>
+
+
                 </div>
-                <div class="year flex items-center text-[50px] font-bold " dir="rtl" >
-                    <span class="text-dark-brown">23</span><span>20</span>
+
+            </div>
+
+
+            <div class="">
+                <header class="flex flex-wrap justify-between items-center mb-5">
+                    <div
+                        class="bg-subtraction-ltr-png rtl:bg-subtraction-png h-16 w-[80%] flex items-center rtl:pr-8  ltr:pl-8 text-xl">
+                        البيانات المالية الخاصة لعام
+                    </div>
+                    <div class="year flex items-center text-[50px] font-bold " dir="rtl">
+                        <span class="text-dark-brown">22</span><span>20</span>
+                    </div>
+                </header>
+
+                <div class="card w-[80%] flex">
+
+                    <div
+                        class=" mx-4 w-60 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-10">
+                        <div class="flex flex-col items-center ">
+                            <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="../../img/company.png"
+                                 alt="Bonnie image"/>
+                            <h5 class="mb-1 text-center text-xl font-medium text-gray-900 dark:text-white">Bonnie
+                                Green</h5>
+                            <span class="text-sm text-gray-500 dark:text-gray-400">Visual Designer</span>
+                            <div class="flex  space-x-3 text-dark-brown font-bold">
+                                2023
+                            </div>
+                        </div>
+                    </div>
+
+                    <div
+                        class=" mx-4 w-60 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-10">
+                        <div class="flex flex-col items-center ">
+                            <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="../../img/company.png"
+                                 alt="Bonnie image"/>
+                            <h5 class="mb-1 text-center text-xl font-medium text-gray-900 dark:text-white">Bonnie
+                                Green</h5>
+                            <span class="text-sm text-gray-500 dark:text-gray-400">Visual Designer</span>
+                            <div class="flex  space-x-3 text-dark-brown font-bold">
+                                2023
+                            </div>
+                        </div>
+                    </div>
+
+                    <div
+                        class=" mx-4 w-60 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-10">
+                        <div class="flex flex-col items-center ">
+                            <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="../../img/company.png"
+                                 alt="Bonnie image"/>
+                            <h5 class="mb-1 text-center text-xl font-medium text-gray-900 dark:text-white">Bonnie
+                                Green</h5>
+                            <span class="text-sm text-gray-500 dark:text-gray-400">Visual Designer</span>
+                            <div class="flex  space-x-3 text-dark-brown font-bold">
+                                2023
+                            </div>
+                        </div>
+                    </div>
+
+                    <div
+                        class=" mx-4 w-60 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-10">
+                        <div class="flex flex-col items-center ">
+                            <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="../../img/company.png"
+                                 alt="Bonnie image"/>
+                            <h5 class="mb-1 text-center text-xl font-medium text-gray-900 dark:text-white">Bonnie
+                                Green</h5>
+                            <span class="text-sm text-gray-500 dark:text-gray-400">Visual Designer</span>
+                            <div class="flex  space-x-3 text-dark-brown font-bold">
+                                2023
+                            </div>
+                        </div>
+                    </div>
+
+
                 </div>
-            </header>
+            </div>
 
         </div>
+
+
     </section>
 
 </template>
@@ -27,7 +168,6 @@
 
 import Navbar from "@/components/Navbar.vue";
 import ImgBanner from "@/components/ImgBanner.vue";
-
 
 
 </script>
