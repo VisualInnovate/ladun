7c07b8fcfa171fab31551bc259c69bd0951672a6
AmrSaleh2234 <amrsaleh1001@gmail.com>
2023-03-12 15:58:14 +0200

    project details done sections

--- resources/js/pages/StateManagement.vue
+++ resources/js/pages/StateManagement.vue
@@ -1,5 +1,5 @@
 <template>
-    <Navbar class="bg-black fixed z-50 w-full" />
+    <Navbar class="bg-black fixed z-50 w-full"/>
     <div class="relative img-banner-slot pt-32">
         <div class=" overlay absolute top-0 right-0 w-full h-full bg-background-overlay z-20"></div>
         <img class="w-full" src="../../img/real-state-management-banner.png">
@@ -121,7 +121,8 @@
 
     </section>
 
-    <section class="relative bg-real-state-managemnt-utility-section bg-cover md:px-16 pt-40 pb-16  text-white flex items-center">
+    <section
+        class="relative bg-real-state-managemnt-utility-section bg-cover  pt-40 pb-16  text-white flex items-center">
         <div class=" overlay absolute top-0 right-0 w-full h-full bg-background-overlay z-20"></div>
         <div class="z-40   w-full">
             <h2 class="mb-5 text-light-brown ">high level utility </h2>
@@ -176,21 +177,37 @@
                     </p>
 
                     <div class="company-button ">
-                        <a class=" special-button text-xl button bg-dark-brown  text-white px-11 py-1 my-5 mb-14 ml-4 rtl:mr-4">
-                            اتصل بنا
+                        <div class="flex flex-wrap justify-between">
+                            <div class="w-1/2 mb-5">
+                                <a class=" special-button text-xl button bg-dark-brown  rounded-lg flex items-center justify-center text-white px-11 py-2 my-5 mb-14 ml-4 rtl:mr-4">
+                                    <ArrowDownTrayIcon class="text-white w-6 h-6 rtl:ml-6 ltr:mr-6"/>
+                                    Project brochure
 
-                        </a>
-                        <a class=" special-button text-xl button bg-dark-brown w-[80px] text-white px-11 py-1 my-5 mb-14 ml-4 rtl:mr-4">
-                            اتصل بنا
+                                </a>
+                            </div>
+                            <div class="w-1/2 mb-5">
+                                <a class=" special-button text-xl button bg-dark-brown rounded-lg  flex items-center justify-center text-white px-11 py-2 my-5 mb-14 ml-4 rtl:mr-4">
+                                    <ArrowDownTrayIcon class="text-white w-6 h-6 rtl:ml-6 ltr:mr-6"/>
+                                    Project brochure
 
-                        </a>
-                        <a class=" special-button text-xl button bg-dark-brown w-[80px] text-white px-11 py-1 my-5 mb-14 ml-4 rtl:mr-4">
-                            اتصل بنا
+                                </a>
+                            </div>
 
-                        </a>
+                            <div class="w-1/2 mb-5">
+                                <a class=" special-button text-xl button bg-dark-brown rounded-lg  flex items-center justify-center text-white px-11 py-2 my-5 mb-14 ml-4 rtl:mr-4">
+                                    <ArrowDownTrayIcon class="text-white w-6 h-6 rtl:ml-6 ltr:mr-6"/>
+                                    Project brochure
+
+                                </a>
+                            </div>
+
+
+
+
+                        </div>
+
 
                     </div>
-
 
 
                 </div>
@@ -206,12 +223,224 @@
 
 
     </section>
+    <section class="company-info   py-28 bg-background-section  ">
 
-    <DarkFooter />
+        <div class="container mx-auto">
+
+            <div class="grid grid-cols-1 gap-12  lg:grid-cols-2 lg:gap-10">
+
+                <div class="d-info h-full flex flex-col justify-center">
+                    <h1 class="font-bold text-3xl mb-4 text-center md:text-start">Project Models </h1>
+                    <tabs variant="underline" v-model="activeTab" class="p-5"> <!-- class appends to content DIV for all tabs -->
+                        <tab name="first" title="Model A">
+                            <div class="text mb-5">
+                                <p>مساحة الأرض : 386 متر مربع</p>
+                                <p>مساحة الأرض : 386 متر مربع</p>
+                                <p>مساحة الأرض : 386 متر مربع</p>
+                            </div>
+                            <div class="flex flex-wrap">
+                                <div class=" ltr:mr-14 rtl:ml-14 flex flex-wrap flex-col items-center justify-center w-[96px]">
+                                    <div class=" p-7 rounded-full border-2 bg-white z-20 mb-2">
+                                        <img src="../../img/icon-utility.png" class="w-[35px] h-[35px] z-40">
+                                    </div>
+
+                                    <p class="text-sm">garden</p>
+                                </div>
+                                <div class=" ltr:mr-14 rtl:ml-14 flex flex-wrap flex-col items-center justify-center w-[96px]">
+                                    <div class=" p-7 rounded-full border-2 bg-white z-20 mb-2">
+                                        <img src="../../img/icon-utility.png" class="w-[35px] h-[35px] z-40">
+                                    </div>
+
+                                    <p class="text-sm">garden</p>
+                                </div>
+                                <div class=" ltr:mr-14 rtl:ml-14 flex flex-wrap flex-col items-center justify-center w-[96px]">
+                                    <div class=" p-7 rounded-full border-2 bg-white z-20 mb-2">
+                                        <img src="../../img/icon-utility.png" class="w-[35px] h-[35px] z-40">
+                                    </div>
+
+                                    <p class="text-sm">garden</p>
+                                </div>
+                                <div class=" ltr:mr-14 rtl:ml-14 flex flex-wrap flex-col items-center justify-center w-[96px]">
+                                    <div class=" p-7 rounded-full border-2 bg-white z-20 mb-2">
+                                        <img src="../../img/icon-utility.png" class="w-[35px] h-[35px] z-40">
+                                    </div>
+
+                                    <p class="text-sm">garden</p>
+                                </div>
+
+
+                            </div>
+                        </tab>
+                        <tab name="second" title="Model B">
+                            <div class="text mb-5">
+                                <p>مساحة الأرض : 386 متر مربع</p>
+                                <p>مساحة الأرض : 386 متر مربع</p>
+                                <p>مساحة الأرض : 386 متر مربع</p>
+                            </div>
+
+                            <div class="flex flex-wrap">
+                                <div class=" ltr:mr-14 rtl:ml-14 flex flex-wrap flex-col items-center justify-center w-[96px]">
+                                    <div class=" p-7 rounded-full border-2 bg-white z-20 mb-2">
+                                        <img src="../../img/icon-utility.png" class="w-[35px] h-[35px] z-40">
+                                    </div>
+
+                                    <p class="text-sm">garden</p>
+                                </div>
+                                <div class=" ltr:mr-14 rtl:ml-14 flex flex-wrap flex-col items-center justify-center w-[96px]">
+                                    <div class=" p-7 rounded-full border-2 bg-white z-20 mb-2">
+                                        <img src="../../img/icon-utility.png" class="w-[35px] h-[35px] z-40">
+                                    </div>
+
+                                    <p class="text-sm">garden</p>
+                                </div>
+                                <div class=" ltr:mr-14 rtl:ml-14 flex flex-wrap flex-col items-center justify-center w-[96px]">
+                                    <div class=" p-7 rounded-full border-2 bg-white z-20 mb-2">
+                                        <img src="../../img/icon-utility.png" class="w-[35px] h-[35px] z-40">
+                                    </div>
+
+                                    <p class="text-sm">garden</p>
+                                </div>
+                                <div class=" ltr:mr-14 rtl:ml-14 flex flex-wrap flex-col items-center justify-center w-[96px]">
+                                    <div class=" p-7 rounded-full border-2 bg-white z-20 mb-2">
+                                        <img src="../../img/icon-utility.png" class="w-[35px] h-[35px] z-40">
+                                    </div>
+
+                                    <p class="text-sm">garden</p>
+                                </div>
+
+
+                            </div>
+                        </tab>
+                        <tab name="third" title="Model C">
+                            <div class="text mb-5">
+                                <p>مساحة الأرض : 386 متر مربع</p>
+                                <p>مساحة الأرض : 386 متر مربع</p>
+                                <p>مساحة الأرض : 386 متر مربع</p>
+                            </div>
+                            <div class="flex flex-wrap">
+                                <div class=" ltr:mr-14 rtl:ml-14 flex flex-wrap flex-col items-center justify-center w-[96px]">
+                                    <div class=" p-7 rounded-full border-2 bg-white z-20 mb-2">
+                                        <img src="../../img/icon-utility.png" class="w-[35px] h-[35px] z-40">
+                                    </div>
+
+                                    <p class="text-sm">garden</p>
+                                </div>
+                                <div class=" ltr:mr-14 rtl:ml-14 flex flex-wrap flex-col items-center justify-center w-[96px]">
+                                    <div class=" p-7 rounded-full border-2 bg-white z-20 mb-2">
+                                        <img src="../../img/icon-utility.png" class="w-[35px] h-[35px] z-40">
+                                    </div>
+
+                                    <p class="text-sm">garden</p>
+                                </div>
+                                <div class=" ltr:mr-14 rtl:ml-14 flex flex-wrap flex-col items-center justify-center w-[96px]">
+                                    <div class=" p-7 rounded-full border-2 bg-white z-20 mb-2">
+                                        <img src="../../img/icon-utility.png" class="w-[35px] h-[35px] z-40">
+                                    </div>
+
+                                    <p class="text-sm">garden</p>
+                                </div>
+                                <div class=" ltr:mr-14 rtl:ml-14 flex flex-wrap flex-col items-center justify-center w-[96px]">
+                                    <div class=" p-7 rounded-full border-2 bg-white z-20 mb-2">
+                                        <img src="../../img/icon-utility.png" class="w-[35px] h-[35px] z-40">
+                                    </div>
+
+                                    <p class="text-sm">garden</p>
+                                </div>
+
+
+                            </div>
+                        </tab>
+
+                    </tabs>
+                </div>
+
+                <div class="img-company-info  ">
+                    <Carousel :pictures="gallery" class="about-us-carousel h-[690px] [&>div:first-child]:h-[690px]  [&>div>div>img]:h-[690px] [&>div>button]:mx-2 [&>div>button]:w-10 [&>button>span]:group-focus:ring-black [&>button>span]:group-focus:ring-1  "/>
+
+                </div>
+
+
+            </div>
+        </div>
+
+
+    </section>
+
+
+    <section class="company-info   py-28 bg-background-section ">
+
+        <div class="container mx-auto">
+
+            <div class="grid grid-cols-1 gap-12  lg:grid-cols-2 lg:gap-10">
+
+                <div class="d-info h-full flex flex-col justify-center">
+                    <h1 class="font-bold text-3xl mb-4 text-center md:text-start">Location </h1>
+                    <p class="mb-24 text-3xl text-dark-brown">
+                        في قلب المدينه
+
+                    </p>
+
+                    <p class="flex " dir="rtl">
+                        <MapPinIcon class="w-6 h-6 text-black"/>Riyadh, Saudi Arabia
+                    </p>
+
+                </div>
+
+                <div class="img-company-info  ">
+                    <iframe class="w-full h-[550px]" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3625.312148166532!2d46.668574651149!3d24.681794158394528!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e2f034b4ad55d0b%3A0x9724c2fb9fb4fce6!2sLadun%20Investment!5e0!3m2!1sen!2seg!4v1678629276760!5m2!1sen!2seg"  style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+
+
+            </div>
+        </div>
+
+
+    </section>
+
+
+
+
+    <DarkFooter/>
 </template>
 
 <script setup>
 import Navbar from '../components/Navbar.vue';
 import DarkFooter from '../components/DarkFooter.vue';
+import { ArrowDownTrayIcon} from "@heroicons/vue/24/solid";
+
+import { Tabs, Tab } from 'flowbite-vue'
+const activeTab = ref('first')
+
+import { Carousel } from 'flowbite-vue'
+import { PhoneIcon , PlayCircleIcon , MapPinIcon} from "@heroicons/vue/24/solid";
+import {ref} from 'vue'
+
+const gallery= ref([
+    {
+        src: new URL('../../img/about-us-info.png', import.meta.url).href,
+        alt: 'project_1'
+    },
+    {
+        src: new URL('../../img/about_ladun_image.png', import.meta.url).href,
+        alt: 'project_2'
+    },
+    {
+        src: new URL('../../img/about-us-info.png', import.meta.url).href,
+        alt: 'project_2'
+    },
+    {
+        src: new URL('../../img/about_ladun_image.png', import.meta.url).href,
+        alt: 'project_1'
+    },
+    {
+        src: new URL('../../img/about-us-info.png', import.meta.url).href,
+        alt: 'project_2'
+    },
+    {
+        src: new URL('../../img/about_ladun_image.png', import.meta.url).href,
+        alt: 'project_2'
+    },
+]);
+
 
 </script>
