b666a8fbc3bc292468c7b09fe99342d3606d1f2b
AmrSaleh2234 <amrsaleh@gmail.com>
2023-03-18 20:13:55 +0200

    .

--- resources/js/pages/MediaCenter.vue
+++ resources/js/pages/MediaCenter.vue
@@ -41,27 +41,14 @@ import axios from "axios";
 
 const router = useRouter()
 const media = ref({})
-const projects_1 = new URL('../../img/banner-mediaCenter.png', import.meta.url).href
 const projects_2 = new URL('../../img/real-state-management-banner.png', import.meta.url).href
-const projects_3 = new URL('../../img/banner-mediaCenter.png', import.meta.url).href
-const projects_4 = new URL('../../img/real-managent-utility-section.png', import.meta.url).href
 const pictures = [
-    {
-        src: projects_1,
-        alt: 'Picture 1',
-    },
-    {
-        src: projects_4,
-        alt: 'Picture 4',
-    },
+
     {
         src: projects_2,
         alt: 'Picture 2',
     },
-    {
-        src: projects_3,
-        alt: 'Picture 3',
-    },]
+  ]
 
 
     const details=(id)=>{
