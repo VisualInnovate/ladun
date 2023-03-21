c5a4294c164640e3f4bd58abd93de263efb3bbba
Ali Ragab <ali@visualinnovate.com>
2023-03-21 15:47:52 +0200

    fixed padding between nav items

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -32,7 +32,7 @@
                         text="Bottom"
                         class="[&>div:nth-child(2n)>ul]:bg-[#51586B] [&>div:nth-child(2n)>ul]:w-100 [&>div:nth-child(2n)>ul]:p-2 [&>div:nth-child(2n)>ul]:box-border [&>div:nth-child(2n)]:bg-[#51586B] hover:[&>div:nth-child(2n)>ul>a>li]:bg-black [&>div:nth-child(2n)>ul>a>li]:text-[#D9D9D9] [&>div:nth-child(2n)>ul>a>li]:inline-block [&>div:nth-child(2n)>ul>a>li]:mx-0 [&>div:nth-child(2n)>ul>a>li]:text-start [&>div:nth-child(2n)>ul>a>li]:border-none [&>div:nth-child(2n)]:rounded-lg hover:cursor-pointer focus:outline-none  font-normal leading-6 text-white relative inline-block after:content-[''] after:absolute after:w-full after:h-[1px] after:rounded-lg after:bg-white after:-bottom-1 after:left-0 after:origin-bottom-right after:scale-x-0 after:transition after:ease-linear after:duration-200 hover:after:origin-bottom-left hover:after:scale-x-100">
                         <template #trigger class="w-full">
-                            <div class="flex justify-between px-4 text-white">
+                            <div class="flex justify-between text-white">
                                 <div>{{ $t(item.name) }}</div>
                             </div>
                         </template>
