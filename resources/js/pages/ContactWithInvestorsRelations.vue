23241f0d01052389b45bc7d8f9b2cdc8a4d17a86
Ali Ragab <ali@visualinnovate.com>
2023-03-21 12:59:27 +0200

    edit navbar and added new contact with investors relations page

---
+++ resources/js/pages/ContactWithInvestorsRelations.vue
@@ -0,0 +1,45 @@
+<template>
+    <Navbar class="bg-black fixed z-50 w-full" />
+    <div class="pt-22">
+        <img-banner>
+            <img  src="../../img/investors-page.png">
+            <template #text>{{$t('investor_banner')}}  </template>
+
+        </img-banner>
+    </div>
+
+    <LightFooter />
+
+</template>
+
+<script>
+import Navbar from "@/components/Navbar.vue";
+import ImgBanner from "@/components/ImgBanner.vue";
+import CardLink from "@/components/CardLink.vue";
+import LightFooter from "../components/LightFooter.vue";
+
+export default {
+    data() {
+      return {
+          investors:[]
+      }
+    },
+    components:{
+        Navbar,ImgBanner,CardLink,LightFooter
+    },
+    methods:{
+        getAllInvestors(){
+            axios.get("/api/investors").then(res =>{
+                this.investors=res.data.investors
+                console.log(this.investors)
+
+            })
+        }
+    },
+    mounted() {
+        this.getAllInvestors()
+
+    }
+}
+
+</script>
