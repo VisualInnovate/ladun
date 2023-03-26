6f37d835c932eca9a8a4751950382b6e56a517ff
AmrSaleh2234 <amrsaleh@gmail.com>
2023-03-26 15:07:36 +0200

    new build

--- resources/js/pages/Project.vue
+++ resources/js/pages/Project.vue
@@ -341,7 +341,7 @@
                 <div class="img-company-info  left">
                     <iframe
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d8622.087051923865!2d46.67816517503699!3d24.68767766179772!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e2f034b4ad55d0b%3A0x9724c2fb9fb4fce6!2sLadun%20Investment!5e0!3m2!1sen!2seg!4v1679391697436!5m2!1sen!2seg"
-                        width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+                        width="500" height="340" style="border:0;" allowfullscreen="" loading="lazy"
                         referrerpolicy="no-referrer-when-downgrade"></iframe>
 
 
