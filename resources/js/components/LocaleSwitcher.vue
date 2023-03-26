0fb0f28a8480f53a577390fb89301f28bdef9b3b
Ali Ragab <ali@visualinnovate.com>
2023-03-26 14:07:12 +0200

    localswitcher logic

--- resources/js/components/LocaleSwitcher.vue
+++ resources/js/components/LocaleSwitcher.vue
@@ -1,6 +1,6 @@
 <script setup>
 
-import {computed} from 'vue'
+import {computed, onBeforeMount} from 'vue'
 import i18n from '../plugins/i18n';
 import {Dropdown, ListGroup, ListGroupItem} from 'flowbite-vue'
 import { ref } from "vue";
@@ -30,7 +30,6 @@ const changeLocale = () => {
         currentLang.value = "العربية"
         secondLang.value = "English"
         executeEnCode()
-
     }
 
 
@@ -49,18 +48,25 @@ const executeEnCode = () => {
     appLang.setAppRtl(true)
 
 }
+
+onBeforeMount(()=>{
+    if (i18n.global.locale.value == 'ar') {
+        secondLang.value = "English"
+        currentLang.value = "العربية"
+    } else {
+        secondLang.value = "العربية"
+        currentLang.value = "English"
+    }
+})
 </script>
 
 <template>
-
-
     <dropdown
-
         class="[&>div:nth-child(2n)>ul]:bg-[#51586B] [&>div:nth-child(2n)>ul]:w-100 [&>div:nth-child(2n)>ul]:p-2 [&>div:nth-child(2n)>ul]:box-border [&>div:nth-child(2n)]:bg-[#51586B] hover:[&>div:nth-child(2n)>ul>li]:bg-black [&>div:nth-child(2n)>ul>li]:text-[#D9D9D9] [&>div:nth-child(2n)>ul>li]:inline-block [&>div:nth-child(2n)>ul>li]:mx-0 [&>div:nth-child(2n)>ul>li]:text-start [&>div:nth-child(2n)>ul>li]:border-none [&>div:nth-child(2n)]:rounded-lg hover:cursor-pointer focus:outline-none  font-normal leading-6 text-white relative inline-block after:content-[''] after:absolute after:w-full after:h-[1px] after:rounded-lg after:bg-white after:-bottom-1 after:left-0 after:origin-bottom-right after:scale-x-0 after:transition after:ease-linear after:duration-200 hover:after:origin-bottom-left hover:after:scale-x-100">
         <template #trigger class="w-full">
             <div class="flex items-center text-white">
-                <div class="px-3">{{ currentLang }}</div>
-                <svg class="w-4 h-4 ml-2" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                <div >{{ currentLang }}</div>
+                <svg class="w-4 h-4 ltr:ml-2 rtl:mr-2" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24"
                      xmlns="http://www.w3.org/2000/svg">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
                 </svg>
@@ -69,9 +75,7 @@ const executeEnCode = () => {
         </template>
         <list-group class="border-none">
             <list-group-item @click="changeLocale">
-
                 {{ secondLang }}
-
             </list-group-item>
         </list-group>
     </dropdown>
