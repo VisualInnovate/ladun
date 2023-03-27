b0c02a648f2a4bfa5282b59105b12f02e1bfdd3c
AmrSaleh2234 <amrsaleh1001@gmail.com>
2023-03-27 13:31:57 +0200

    active link in navbar

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -33,7 +33,7 @@
                         class="[&>div:nth-child(2n)>ul]:bg-[#51586B] [&>div:nth-child(2n)>ul]:w-100 [&>div:nth-child(2n)>ul]:p-2 [&>div:nth-child(2n)>ul]:box-border [&>div:nth-child(2n)]:bg-[#51586B] hover:[&>div:nth-child(2n)>ul>a>li]:bg-black [&>div:nth-child(2n)>ul>a>li]:text-[#D9D9D9] [&>div:nth-child(2n)>ul>a>li]:inline-block [&>div:nth-child(2n)>ul>a>li]:mx-0 [&>div:nth-child(2n)>ul>a>li]:text-start [&>div:nth-child(2n)>ul>a>li]:border-none [&>div:nth-child(2n)]:rounded-lg hover:cursor-pointer focus:outline-none  font-normal leading-6 text-white relative inline-block after:content-[''] after:absolute after:w-full after:h-[1px] after:rounded-lg after:bg-white after:-bottom-1 after:left-0 after:origin-bottom-right after:scale-x-0 after:transition after:ease-linear after:duration-200 hover:after:origin-bottom-left hover:after:scale-x-100">
                         <template #trigger class="w-full">
                             <div class="flex justify-between text-white">
-                                <div>{{ $t(item.name) }}</div>
+                                <div :id="item.name">{{ $t(item.name) }}</div>
                             </div>
                         </template>
                         <list-group class="border-none">
@@ -139,7 +139,7 @@
 </template>
 
 <script setup>
-import {ref} from "vue";
+import {ref , onMounted } from "vue";
 import {Dialog, DialogPanel} from "@headlessui/vue";
 import {Bars3Icon, XMarkIcon} from "@heroicons/vue/24/outline";
 import LocaleSwitcher from "./LocaleSwitcher.vue";
@@ -186,6 +186,21 @@ const navigation = [
     {name: "joinUs", href: {name: 'Join Us'}, dropdown: false},
 ];
 const mobileMenuOpen = ref(false);
+onMounted(()=>{
+    var pathArray = window.location.pathname.split('/');
+    if (pathArray[1]== "about" || pathArray[1]== "highboard" || pathArray[1]== "media-center" ) {
+        document.getElementById('aboutUs').style['color']='#eeb013'
+    }
+
+    if (pathArray[1]== "financials" || pathArray[1]== "investors-ads" || pathArray[1]== "investors-relations-contact" ) {
+        document.getElementById('investorsRelations').style['color']='#eeb013'
+    }
+
+
+
+})
+
+
 </script>
 
 <style lang="scss" scoped>
