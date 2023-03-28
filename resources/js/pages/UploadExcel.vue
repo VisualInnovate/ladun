7530e949fe15d4ae963482137444921ee1335fdd
AmrSaleh2234 <amrsaleh@gmail.com>
2023-03-28 14:17:30 +0200

    new build

--- resources/js/pages/UploadExcel.vue
+++ resources/js/pages/UploadExcel.vue
@@ -1,13 +1,13 @@
 <template>
-    <div class="container mx-auto">
-        <div v-if="success!='' " class="p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400" role="alert">
+    <div class="container mx-auto git mt-10">
+        <div v-if="success!='' " class="  p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400" role="alert">
             {{success}}
         </div>
         <div class="flex flex-col items-center">
             <label class="font-bold"> upload file project </label>
-            <input @change="handleFile" type="file" placeholder="upload file project" ref="file">
+            <input @change="handleFile" type="file" placeholder="upload file project" ref="file" class=" m-5 block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" >
             <div class="text-red-500 " v-if="error.file">{{error.cv[0]}}</div>
-            <button type="submit" class="text-white bg-dark-brown p-3" @click="submit">
+            <button type="submit" class="text-white bg-dark-brown p-3 rounded-lg" @click="submit">
                 submit
             </button>
         </div>
