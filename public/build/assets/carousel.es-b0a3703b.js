911d8f0ae76d7308b4f6d008e52058a0bc4b64a4
Ali Ragab <ali@visualinnovate.com>
2023-03-28 16:57:46 +0200

    build

--- public/build/assets/carousel.es-b0a3703b.js
+++ public/build/assets/carousel.es-b0a3703b.js
@@ -1,4 +1,4 @@
-import{B as te,y as m,g as u,x as p,H as I,C as be,J as xe,E as ye,A as Q,D as K,v as fe,G as y}from"./main-745a73bb.js";/**
+import{B as te,y as m,g as u,x as p,H as I,C as be,J as xe,E as ye,A as Q,D as K,v as fe,G as y}from"./main-2441d201.js";/**
  * Vue 3 Carousel 0.2.12
  * (c) 2023
  * @license MIT
