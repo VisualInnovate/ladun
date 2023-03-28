921410a452e9ac994ead55534859bce8aae5958b
AmrSaleh2234 <amrsaleh@gmail.com>
2023-03-28 09:50:05 +0200

    .

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -8,8 +8,7 @@
                 <button
                     type="button"
                     class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-                    @click="mobileMenuOpen = true"
-                >
+                    @click="mobileMenuOpen = true">
                     <span class="sr-only">Open main menu</span>
                     <Bars3Icon v-show="!mobileMenuOpen" class="h-6 w-6 text-white" aria-hidden="true"/>
                 </button>
