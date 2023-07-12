be0004112ec025d5d4df7c94aea174b354970a89
Mohamed <moahmedpro1@gmail.com>
2023-07-12 15:06:38 +0200

    fildes

--- resources/js/pages/ContactUs.vue
+++ resources/js/pages/ContactUs.vue
@@ -43,14 +43,13 @@
                     >
                         <div class="flex justify-center gap-x-5">
                             <input
-                            type="text"
-                            id="first_name" maxlength="40" name="first_name"
-                            aria-describedby="helper-text-explanation"
-                            v-model="first_name"
-                            class="w-full md:w-1/2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-dark-brown focus:border-dark-brown block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-dark-brown dark:focus:border-dark-brown"
-                            :placeholder="$t('FirstName')"
-                        />
-                            
+                                type="text"
+                                id="first_name" maxlength="40" name="first_name"
+                                aria-describedby="helper-text-explanation"
+                                v-model="first_name"
+                                class="w-full md:w-1/2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-dark-brown focus:border-dark-brown block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-dark-brown dark:focus:border-dark-brown"
+                                :placeholder="$t('FirstName')"
+                            />
                            
                             <input
                             type="text"
@@ -70,7 +69,6 @@
                                    type="hidden"/>  <!-- Object Of Interest -->
                             <!-- This field should not appear to the user and shall be filled automatically -->
                             <!-- choose your inquiry -->
-
                             <input
                             type="text"
                             id="last_name" maxlength="80" name="last_name"
@@ -79,6 +77,7 @@
                             class="w-full md:w-1/2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-dark-brown focus:border-dark-brown block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-dark-brown dark:focus:border-dark-brown"
                             :placeholder="$t('LastName')"
                         />
+                            
                             <input
                                 type="email"
                                 id="email" maxlength="80" name="email"
@@ -92,19 +91,6 @@
                         <div class="flex justify-center gap-x-5">
                             <select
                             class="w-full md:w-1/2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-dark-brown focus:border-dark-brown block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-dark-brown dark:focus:border-dark-brown"
-                            v-model="region"
-                        >
-                            <option value="Eastern"> {{ $t('easternRegion') }}</option>
-                            <option value="Middle"> {{ $t('middleRegion') }}</option>
-                            <option value="Northern"> {{ $t('westernRegion') }}</option>
-                            <option value="Western"> {{ $t('northernRegion') }}</option>
-
-
-                        </select>
-                            
-                          
-                            <select
-                            class="w-full md:w-1/2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-dark-brown focus:border-dark-brown block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-dark-brown dark:focus:border-dark-brown"
                             v-model="status"
                         >
                             <option value="Buy"> {{ $t('ownership') }}</option>
@@ -113,6 +99,20 @@
 
 
                         </select>
+                            <select
+                                class="w-full md:w-1/2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-dark-brown focus:border-dark-brown block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-dark-brown dark:focus:border-dark-brown"
+                                v-model="region"
+                            >
+                                <option value="Eastern"> {{ $t('easternRegion') }}</option>
+                                <option value="Middle"> {{ $t('middleRegion') }}</option>
+                                <option value="Northern"> {{ $t('westernRegion') }}</option>
+                                <option value="Western"> {{ $t('northernRegion') }}</option>
+
+
+                            </select>
+                            
+                           
+                           
                         </div>
 
                         <button
