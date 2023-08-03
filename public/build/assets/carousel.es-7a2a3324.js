d93412f5a485d52c77674593b2e10e251943af9b
AmrSaleh2234 <amrsaleh1001@gmail.com>
2023-08-03 11:12:23 +0300

    new build

--- public/build/assets/carousel.es-7a2a3324.js
+++ public/build/assets/carousel.es-7a2a3324.js
@@ -1,4 +1,4 @@
-import{C as te,z as m,g as u,y as p,I as L,H as y,D as be,K as xe,G as ye,B as Q,E as J,x as fe}from"./main-f5184773.js";/**
+import{C as te,z as m,g as u,y as p,I as L,H as y,D as be,K as xe,G as ye,B as Q,E as J,x as fe}from"./main-430b897f.js";/**
  * Vue 3 Carousel 0.2.12
  * (c) 2023
  * @license MIT
--- resources/js/app.vue
+++ resources/js/app.vue
@@ -2,7 +2,7 @@
     <div class="">
         <RouterView />
 
-        <a href="" class="fixed ltr:right-14 ltr:bottom-16 z-10 " >
+        <a href="https://api.whatsapp.com/send/?phone=920011560&text&type=phone_number&app_absent=0" target="_blank" class="fixed ltr:right-14 ltr:bottom-16 rtl:left-14 rtl:bottom-16 z-10 " >
             <img src="../img/whatsaap.png" class="w-16 h-16 ">
         </a>
     </div>
