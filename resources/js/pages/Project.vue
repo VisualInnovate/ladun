14fc22eb26293016fd38ed6e4913b503a44accc8
AmrSaleh2234 <amrsaleh1001@gmail.com>
2023-09-05 09:38:12 +0300

    new commit

--- resources/js/pages/Project.vue
+++ resources/js/pages/Project.vue
@@ -838,7 +838,7 @@ export default {
             return this.$i18n.locale
         },
         phases() {
-            if(this.recompute==5)
+            if(this.recompute==10)
             {
                 let i=0
                 this.project.phases.forEach((elem)=>{
