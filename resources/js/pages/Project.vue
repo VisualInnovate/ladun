7e21f523bfc460f6a3b70559272120ace7aec212
AmrSaleh2234 <amrsaleh1001@gmail.com>
2023-09-05 12:30:21 +0300

    .

--- resources/js/pages/Project.vue
+++ resources/js/pages/Project.vue
@@ -535,61 +535,6 @@
         <div class="lg:w-[60%] m-auto py-4" style="border-bottom: 2px solid black;"></div>
     </section>
 
-    <section class="company-info   py-28 pt-16  bg-background-section " id="project_models" v-if="project.project_models.length">
-
-        <div class="container mx-auto">
-
-            <div class="">
-
-                <div class="d-info h-full flex flex-col justify-center right">
-                    <h1 class="font-bold text-2xl mb-4 text-center md:text-start">{{ $t('Project_Models') }} </h1>
-                    <tabs variant="underline" v-model="activeTab" class="p-5 text-lg">
-                        <!-- class appends to content DIV for all tabs -->
-                        <tab v-for="model in project.project_models" :name="model.title['en']"
-                             :title="model.title[$i18n.locale]"
-                             class="text-lg [&>div]:grid [&>div]:grid-cols-1 [&>div]:gap-12 [&>div]:lg:grid-cols-2 [&>div]:lg:gap-2">
-                            <div>
-                                <div class="text mb-5">
-                                    <p class="mb-3">{{ $t('Land_Area') }}: {{ model.Land_area }}</p>
-                                    <p class="mb-3">{{ $t('Building_Area') }}: {{ model.building_area }}</p>
-                                    <p class="mb-3">{{ $t('Floors_Number') }}: {{ model.floors_number }}</p>
-                                    <p>{{ $t('Units_Number') }}: {{ model.units_number }}</p>
-                                </div>
-                                <div class="flex flex-wrap">
-                                    <div
-                                        v-if="model.utilities"
-                                        v-for="utility in model.utilities"
-                                        class=" ltr:mr-14 rtl:ml-14 flex flex-wrap flex-col items-center justify-center w-[96px]">
-
-                                        <div class=" p-7 rounded-full border-2 bg-white z-20 mb-2">
-                                            <img v-if="utility.media[0]" :src="utility.media[0].original_url"
-                                                 class="w-[35px] h-[35px] z-40">
-                                            <!--                                            add dinamic img -->
-                                        </div>
-
-                                        <p class="text-sm whitespace-nowrap">{{ utility.title[$i18n.locale] }}</p>
-
-                                    </div>
-
-                                </div>
-
-                            </div>
-
-                            <div class="  left">
-                                <Carousel :pictures="getGallery(model.media)"
-                                          class="about-us-carousel h-[400px] [&>div:first-child]:h-[400px]  [&>div>div>img]:h-[400px] [&>div>button]:mx-2 [&>div>button]:w-10 [&>button>span]:group-focus:ring-black [&>button>span]:group-focus:ring-1  "/>
-
-                            </div>
-
-                        </tab>
-
-                    </tabs>
-                </div>
-
-
-            </div>
-        </div>
-    </section>
     <!-- Video -->
 
     <section id="video" class="bg-[white]">
@@ -692,7 +637,7 @@
                 </svg>
             </div>
             <div class="lg:w-[60%] grid grid-cols-1 lg:grid-cols-3 m-auto">
-                <div class="flex">
+                <a :href="`/storage/${download.project_attachment}`" target="_blank" class="flex" v-for="download in project.downloads">
                     <svg width="44px" height="44px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
                         <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
@@ -704,36 +649,9 @@
                                   stroke-linecap="round" stroke-linejoin="round"></path>
                         </g>
                     </svg>
-                    <p class="text-2xl p-2 font-bold my-auto">الكتيب الترويجي</p>
-                </div>
-                <div class="flex">
-                    <svg width="44px" height="44px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-                        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
-                        <g id="SVGRepo_iconCarrier">
-                            <path
-                                d="M8 22.0002H16C18.8284 22.0002 20.2426 22.0002 21.1213 21.1215C22 20.2429 22 18.8286 22 16.0002V15.0002C22 12.1718 22 10.7576 21.1213 9.8789C20.3529 9.11051 19.175 9.01406 17 9.00195M7 9.00195C4.82497 9.01406 3.64706 9.11051 2.87868 9.87889C2 10.7576 2 12.1718 2 15.0002L2 16.0002C2 18.8286 2 20.2429 2.87868 21.1215C3.17848 21.4213 3.54062 21.6188 4 21.749"
-                                stroke="#C4A94C" stroke-width="1.5" stroke-linecap="round"></path>
-                            <path d="M12 2L12 15M12 15L9 11.5M12 15L15 11.5" stroke="#C4A94C" stroke-width="1.5"
-                                  stroke-linecap="round" stroke-linejoin="round"></path>
-                        </g>
-                    </svg>
-                    <p class="text-2xl p-2 font-bold my-auto"> المخطط العام</p>
-                </div>
-                <div class="flex">
-                    <svg width="44px" height="44px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-                        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
-                        <g id="SVGRepo_iconCarrier">
-                            <path
-                                d="M8 22.0002H16C18.8284 22.0002 20.2426 22.0002 21.1213 21.1215C22 20.2429 22 18.8286 22 16.0002V15.0002C22 12.1718 22 10.7576 21.1213 9.8789C20.3529 9.11051 19.175 9.01406 17 9.00195M7 9.00195C4.82497 9.01406 3.64706 9.11051 2.87868 9.87889C2 10.7576 2 12.1718 2 15.0002L2 16.0002C2 18.8286 2 20.2429 2.87868 21.1215C3.17848 21.4213 3.54062 21.6188 4 21.749"
-                                stroke="#C4A94C" stroke-width="1.5" stroke-linecap="round"></path>
-                            <path d="M12 2L12 15M12 15L9 11.5M12 15L15 11.5" stroke="#C4A94C" stroke-width="1.5"
-                                  stroke-linecap="round" stroke-linejoin="round"></path>
-                        </g>
-                    </svg>
-                    <p class="text-2xl p-2 font-bold my-auto"> منشور واتساب</p>
-                </div>
+                    <p class="text-2xl p-2 font-bold my-auto">{{download.name[$i18n.locale]}}</p>
+                </a>
+
             </div>
 
         </div>
