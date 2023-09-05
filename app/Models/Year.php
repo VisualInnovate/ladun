594deb6ccad334f66feb2b4703c50d91539e5d08
VisualInnovate <115986400+VisualInnovate@users.noreply.github.com>
2023-09-05 22:43:03 +0300

    .

---
+++ app/Models/Year.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\Translatable\HasTranslations;
+
+class Year extends  Model implements HasMedia
+{
+    use HasFactory ;
+    use InteractsWithMedia;
+    use HasTranslations;
+    protected $guarded = [];
+    public $translatable = ['title'];
+    protected  $table = 'years_report';
+}
--- resources/js/pages/FinancialData.vue
+++ resources/js/pages/FinancialData.vue
@@ -1,57 +1,116 @@
 <template>
-    <Navbar class="bg-black fixed z-50 w-full" />
+    <Navbar class="bg-black fixed z-50 w-full"/>
     <img-banner>
         <img src="../../img/finncial-banner.png">
-        <template #text>{{$t('finncial_banner')}}</template>
+        <template #text>{{ $t('finncial_banner') }}</template>
 
     </img-banner>
 
-    <section class="media-center py-28 bg-background-section pt-32" >
+    <section class="media-center py-28 bg-background-section pt-32">
         <div class="container mx-auto">
-<!--            {{financials}}-->
-            <div v-for="(financial,index ) in financials" :key="index" class="mb-12">
+            <!--            {{financials}}-->
+            <div v-for="f  in financials" :key="index" class="mb-6">
+                <div v-for="(financial,index ) in f">
 
-                <header class="flex flex-wrap justify-between items-center mb-5  " :class="($i18n.locale=='en' )  ? 'animate__animated animate__fadeInLeft' : 'animate__animated animate__fadeInRight'">
-                    <div
-                        class="ltr:bg-subtraction-ltr-png rtl:bg-subtraction-png h-16 w-[80%] flex items-center rtl:pr-8  ltr:pl-8 text-xl">
-                        {{$t('header_finncial')}}
-                    </div>
-                    <div class="year flex items-center text-[50px] font-bold " dir="rtl">
-                        <span class="text-dark-brown">{{index[2] }}{{index[3]}}</span><span>{{index[0] }}{{index[1]}}</span>
-                    </div>
-                </header>
-
-                <div  class="card w-[80%] flex">
-
-                    <a v-for="item in financial"
-                       :href="'storage/'+item.financial_file"
-
-                        class="animate__animated animate__bounceIn mx-4 w-60 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-10">
-                        <div class="flex flex-col items-center ">
-                            <div class=" w-24 h-24 mb-3 rounded-full p-5 shadow-lg flex justify-center items-center">
-                                <img class=" w-12 h-12 " src="../../img/financial-logo.png"
-                                      alt="Bonnie image"/>
-                            </div>
-                            <h5 class="mb-1 text-center text-xl font-medium text-gray-900 dark:text-white">{{$t('list_money')}}</h5>
-                            <span class="text-sm text-gray-500 dark:text-gray-400">{{item.title[$i18n.locale]}}</span>
-                            <div class="flex  space-x-3 text-dark-brown font-bold">
-                                {{index}}
-                            </div>
+                    <header class="flex flex-wrap justify-between items-center mb-5  "
+                            :class="($i18n.locale=='en' )  ? 'animate__animated animate__fadeInLeft' : 'animate__animated animate__fadeInRight'">
+                        <div
+                            class="ltr:bg-subtraction-ltr-png rtl:bg-subtraction-png h-16 w-[80%] flex items-center rtl:pr-8  ltr:pl-8 text-xl">
+                            {{ $t('header_finncial') }}
                         </div>
-                    </a>
+                        <div class="year flex items-center text-[50px] font-bold " dir="rtl">
+                            <span class="text-dark-brown">{{ index[2] }}{{ index[3] }}</span><span>{{
+                                index[0]
+                            }}{{ index[1] }}</span>
+                        </div>
+                    </header>
 
+                    <div class="card w-[80%] flex">
+
+                        <a v-for="item in financial"
+                           :href="'storage/'+item.financial_file"
+
+                           class="animate__animated animate__bounceIn mx-4 w-60 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-10">
+                            <div class="flex flex-col items-center ">
+                                <div
+                                    class=" w-24 h-24 mb-3 rounded-full p-5 shadow-lg flex justify-center items-center">
+                                    <img class=" w-12 h-12 " src="../../img/financial-logo.png"
+                                         alt="Bonnie image"/>
+                                </div>
+                                <h5 class="mb-1 text-center text-xl font-medium text-gray-900 dark:text-white">
+                                    {{ $t('list_money') }}
+                                </h5>
+                                <span
+                                    class="text-sm text-gray-500 dark:text-gray-400">{{ item.title[$i18n.locale] }}</span>
+                                <div class="flex  space-x-3 text-dark-brown font-bold">
+                                    {{ index }}
+                                </div>
+                            </div>
+                        </a>
+
+                    </div>
                 </div>
             </div>
+
+  <div v-for="f  in years" :key="index" class="mb-6">
+                <div v-for="(year,index ) in f">
+
+                    <header class="flex flex-wrap justify-between items-center mb-5  "
+                            :class="($i18n.locale=='en' )  ? 'animate__animated animate__fadeInLeft' : 'animate__animated animate__fadeInRight'">
+                        <div
+                            class="ltr:bg-subtraction-ltr-png rtl:bg-subtraction-png h-16 w-[80%] flex items-center rtl:pr-8  ltr:pl-8 text-xl">
+                            {{ $t('header_finncial') }}
+                        </div>
+                        <div class="year flex items-center text-[50px] font-bold " dir="rtl">
+                            <span class="text-dark-brown">{{ index[2] }}{{ index[3] }}</span><span>{{
+                                index[0]
+                            }}{{ index[1] }}</span>
+                        </div>
+                    </header>
+
+                    <div class="card w-[80%] flex">
+
+                        <a v-for="item in year"
+                           :href="'storage/'+item.year_file"
+
+                           class="animate__animated animate__bounceIn mx-4 w-60 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-10">
+                            <div class="flex flex-col items-center ">
+                                <div
+                                    class=" w-24 h-24 mb-3 rounded-full p-5 shadow-lg flex justify-center items-center">
+                                    <img class=" w-12 h-12 " src="../../img/financial-logo.png"
+                                         alt="Bonnie image"/>
+                                </div>
+                                <h5 class="mb-1 text-center text-xl font-medium text-gray-900 dark:text-white">
+                                    {{ $t('list_money') }}
+                                </h5>
+                                <span
+                                    class="text-sm text-gray-500 dark:text-gray-400">{{ item.title[$i18n.locale] }}</span>
+                                <div class="flex  space-x-3 text-dark-brown font-bold">
+                                    {{ index }}
+                                </div>
+                            </div>
+                        </a>
+
+                    </div>
+                </div>
+            </div>
+
+
+
+
+
+
+
         </div>
 
 
     </section>
 
-    <LightFooter />
+    <LightFooter/>
 
 </template>
 
-<script >
+<script>
 
 import Navbar from "@/components/Navbar.vue";
 import ImgBanner from "@/components/ImgBanner.vue";
@@ -65,41 +124,59 @@ export default {
             return i18n
         }
     },
-    components:{
-      Navbar ,ImgBanner,LightFooter
+    components: {
+        Navbar, ImgBanner, LightFooter
     },
-    data(){
-        return{
-            financials:[]
+    data() {
+        return {
+            financials: [],
+              years: []
         }
     },
-    methods:{
-        getAllFincials(){
-            axios.get("/api/financials").then(res =>{
-                this.financials=res.data.financials
-                // const reverseBaseonValues = Object.values(res.data.financials).reverse();
-                // var reverseBaseonKeys = Object.keys(res.data.financials).reverse();
-                // // console.log(reverseBaseonKeys)
-                // var i = 0;
-                //
-                // reverseBaseonValues.forEach((elem)=>{
-                //     let obj = {}
-                //     obj[reverseBaseonKeys[i]]=elem
-                //     this.financials.push(obj);
-                //     i+=1
-                // }
-            // )
-                // this.financials.map((elem)=>{
-                //     elem
-                // })
+    methods: {
+        getAllFincials() {
+            axios.get("/api/financials").then(res => {
+                // this.financials=res.data.financials
+                const reverseBaseonValues = Object.values(res.data.financials).reverse();
+                var reverseBaseonKeys = Object.keys(res.data.financials).reverse();
+                // console.log(reverseBaseonKeys)
+                var i = 0;
+
+                reverseBaseonValues.forEach((elem) => {
+                        let obj = {}
+                        obj[reverseBaseonKeys[i]] = elem
+                        this.financials.push(obj);
+                        i += 1
+                    }
+                )
                 console.log(this.financials)
+
+            })
+        },
+         getAllYears() {
+            axios.get("/api/years").then(res => {
+                // this.years=res.data.years
+                const reverseBaseonValues = Object.values(res.data.years).reverse();
+                var reverseBaseonKeys = Object.keys(res.data.years).reverse();
+                // console.log(reverseBaseonKeys)
+                var i = 0;
+
+                reverseBaseonValues.forEach((elem) => {
+                        let obj = {}
+                        obj[reverseBaseonKeys[i]] = elem
+                        this.years.push(obj);
+                        i += 1
+                    }
+                )
+
+                console.log(this.years)
 
             })
         }
     },
     mounted() {
-        this.getAllFincials()
-
+        this.getAllFincials(),
+        this.getAllYears()
     }
 }
 
