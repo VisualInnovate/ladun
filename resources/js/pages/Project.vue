c4f8ef4e63c4a04fec9a5fee49581592cae16d58
AmrSaleh2234 <amrsaleh1001@gmail.com>
2023-09-06 16:51:01 +0300

    new commmit

--- resources/js/pages/Project.vue
+++ resources/js/pages/Project.vue
@@ -408,8 +408,8 @@
     <section id="serves" class="bg-[white]  py-[3%]">
         <div class="container  mx-auto">
             <div class="flex">
-                <svg width="30px" height="30px" viewBox="0 0 1024 1024" class="icon px-1 my-auto" version="1.1" style="color: rgba(25,83,177,1)"
-                     xmlns="http://www.w3.org/2000/svg" fill="#213350">
+                <svg width="30px" height="30px" viewBox="0 0 1024 1024" class="icon px-1 my-auto" version="1.1"
+                     xmlns="http://www.w3.org/2000/svg" fill="#000000">
                     <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
                     <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
                     <g id="SVGRepo_iconCarrier">
@@ -435,7 +435,7 @@
                 </div>
                 <div style="border-bottom:1px solid rgb(76, 73, 73);" class="m-auto flex w-full">
                     <svg width="64px" height="64px" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"
-                         fill="rgba(25,83,177,1)" class="bi bi-signpost-split">
+                         fill="#000000" class="bi bi-signpost-split">
                         <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
                         <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
                         <g id="SVGRepo_iconCarrier">
@@ -446,7 +446,7 @@
                     <p class="mx-4 text-2xl"> {{ $t('sidewalks') }} <br>{{ $t('roads') }}</p>
                 </div>
                 <div style="border-bottom:1px solid rgb(76, 73, 73);" class="m-auto flex w-full">
-                    <svg width="64px" height="64px" viewBox="0 0 1024 1024" fill="rgba(25,83,177,1)" class="icon" version="1.1"
+                    <svg width="64px" height="64px" viewBox="0 0 1024 1024" fill="#000000" class="icon" version="1.1"
                          xmlns="http://www.w3.org/2000/svg">
                         <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
                         <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
@@ -802,22 +802,11 @@
         <div class="lg:w-[60%] m-auto py-4" style="border-bottom: 2px solid black;"></div>
     </section>
 
-    <section class=" company-info  bg-[white]" v-if="project.partner_image != '' && project.partner_image">
+    <section class=" company-info  bg-[white] mb-5" v-if="project.partner_image != '' && project.partner_image">
 
-        <div class="container mx-auto" >
+        <div class="container mx-auto mt-8" >
             <div class="flex pb-[5%]">
-                <p class="text-2xl my-auto px-2 text-dark-brown">{{ $t("partners") }}</p>
-                <svg width="44px" height="44px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-                    <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
-                    <g id="SVGRepo_iconCarrier">
-                        <path
-                            d="M8 22.0002H16C18.8284 22.0002 20.2426 22.0002 21.1213 21.1215C22 20.2429 22 18.8286 22 16.0002V15.0002C22 12.1718 22 10.7576 21.1213 9.8789C20.3529 9.11051 19.175 9.01406 17 9.00195M7 9.00195C4.82497 9.01406 3.64706 9.11051 2.87868 9.87889C2 10.7576 2 12.1718 2 15.0002L2 16.0002C2 18.8286 2 20.2429 2.87868 21.1215C3.17848 21.4213 3.54062 21.6188 4 21.749"
-                            stroke="#C4A94C" stroke-width="1.5" stroke-linecap="round"></path>
-                        <path d="M12 2L12 15M12 15L9 11.5M12 15L15 11.5" stroke="#C4A94C" stroke-width="1.5"
-                              stroke-linecap="round" stroke-linejoin="round"></path>
-                    </g>
-                </svg>
+                <p class="text-2xl my-auto px-2 text-dark-brown ">{{ $t("partners") }}</p>
             </div>
             <div class="lg:w-[90%]  mx-auto">
                 <img :src="project.partner_image.original_url" style="width: 70%;height:300px">
