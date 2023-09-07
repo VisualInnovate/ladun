4220b5846308a4d85cda7f0b2f35d0bd5a50a1b3
AmrSaleh2234 <amrsaleh1001@gmail.com>
2023-09-07 11:27:23 +0300

    new commit]

--- public/build/assets/carousel.es-696bcbb8.js
+++ public/build/assets/carousel.es-696bcbb8.js
@@ -1,4 +1,4 @@
-import{C as te,z as m,g as u,y as p,I as L,H as y,D as be,K as xe,G as ye,B as Q,E as J,x as fe}from"./main-ede8d01e.js";/**
+import{C as te,z as m,g as u,y as p,I as L,H as y,D as be,K as xe,G as ye,B as Q,E as J,x as fe}from"./main-388fa8ca.js";/**
  * Vue 3 Carousel 0.2.12
  * (c) 2023
  * @license MIT
--- resources/js/pages/Projects.vue
+++ resources/js/pages/Projects.vue
@@ -35,6 +35,7 @@
                                <div class="flex">
                                 <h3 class="flex-initial my-auto text-[#636463] p-2 font-bold">{{ project.title[$i18n.locale] }}</h3>
                                 <svg class="my-auto" width="25px" height="25px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M12 21C15.5 17.4 19 14.1764 19 10.2C19 6.22355 15.866 3 12 3C8.13401 3 5 6.22355 5 10.2C5 14.1764 8.5 17.4 12 21Z" stroke="#AF9744" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M12 12C13.1046 12 14 11.1046 14 10C14 8.89543 13.1046 8 12 8C10.8954 8 10 8.89543 10 10C10 11.1046 10.8954 12 12 12Z" stroke="#AF9744" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+                                   <h3 class="flex-initial my-auto text-[#636463] p-2 font-bold">{{ project.address[$i18n.locale] }}</h3>
                                </div>
                                 <div class=" mb-4 ">
                                     <p class="px-2 h-24 text-grey text-sm col-start-1 col-end-4 "
@@ -93,7 +94,7 @@
 </template>
 
 <script setup>
-import {ref, onBeforeMount} from "vue";
+import {ref, onBeforeMount, watch} from "vue";
 import Navbar from "../components/Navbar.vue";
 import LightFooter from "../components/LightFooter.vue";
 import { BuildingOffice2Icon} from "@heroicons/vue/24/outline";
@@ -115,7 +116,11 @@ const activeTab = ref('')
 const regionActiveTab = ref('')
 
 const fetchedData = ref([]);
+watch(activeTab , (newValue,oldValue)=>{
+    console.log(newValue)
+    regionActiveTab.value = fetchedData.value[1].regions[0].title['en']
 
+})
 onBeforeMount(async () => {
     const response = await axios.get('/api/departments')
 console.log(response)
