236788733b7a2f22f57e546e239a7bc64c322b0e
AmrSaleh2234 <amrsaleh1001@gmail.com>
2023-09-20 13:52:25 +0300

    new build

--- public/build/assets/carousel.es-3d87eeee.js
+++ public/build/assets/carousel.es-3d87eeee.js
@@ -1,4 +1,4 @@
-import{C as te,z as m,g as u,y as p,I as L,H as y,D as be,K as xe,G as ye,B as Q,E as J,x as fe}from"./main-1052266b.js";/**
+import{C as te,z as m,g as u,y as p,I as L,H as y,D as be,K as xe,G as ye,B as Q,E as J,x as fe}from"./main-37a92931.js";/**
  * Vue 3 Carousel 0.2.12
  * (c) 2023
  * @license MIT
