9ea2885eb2239f1a2568989a554e708258b6a6fb
AmrSaleh2234 <amrsaleh1001@gmail.com>
2023-10-02 15:36:27 +0300

    new build

--- resources/js/pages/FinancialData.vue
+++ resources/js/pages/FinancialData.vue
@@ -26,6 +26,7 @@
 
                                                 </div>
                                             </div>
+                                            {{ reports[0][index] }}
                                             <span class="text-dark-brown" v-if="reports[0][index] == undefined">{{ index[2] }}{{ index[3] }}</span><span>{{
                                                 index[0]
                                             }}{{ index[1] }}</span>
