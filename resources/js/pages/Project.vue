7ac4b2ab90172179cee328f80d2acbb2ae501e94
AmrSaleh2234 <amrsaleh@gmail.com>
2023-10-04 12:16:37 +0300

    new commit

--- resources/js/pages/Project.vue
+++ resources/js/pages/Project.vue
@@ -555,7 +555,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="flex  w-[85%] m-auto " id="carsoul-project">
+                <div class="flex  w-[85%] sm:h-[200px] m-auto " id="carsoul-project">
                     <div class=" w-[1200px]  m-auto">
 
                         <Carousel class="w-full h-full [&>div]:h-[600px] [&>div>div>img]:h-[600px]" :pictures="getGallery(project.gallery)"/>
