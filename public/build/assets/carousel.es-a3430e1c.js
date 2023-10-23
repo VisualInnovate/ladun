fdf55a3dfd2646152b83d5c8a53d16466b29b109
mohamedemadal <m.emad@visualinnovate.com>
2023-10-23 09:13:09 +0300

    new commit

--- public/build/assets/carousel.es-a3430e1c.js
+++ public/build/assets/carousel.es-a3430e1c.js
@@ -1,4 +1,4 @@
-import{C as te,z as m,g as u,y as p,I as L,H as y,D as be,K as xe,G as ye,B as Q,E as J,x as fe}from"./main-b350cee5.js";/**
+import{C as te,z as m,g as u,y as p,I as L,H as y,D as be,K as xe,G as ye,B as Q,E as J,x as fe}from"./main-2070f2c8.js";/**
  * Vue 3 Carousel 0.2.12
  * (c) 2023
  * @license MIT
