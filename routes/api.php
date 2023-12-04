0ecc1c643f46bdb3fdc25e1de98f29eee1f8322e
VisualInnovate <115986400+VisualInnovate@users.noreply.github.com>
2023-12-04 20:57:18 +0200

    Update api.php
    year finanical

--- routes/api.php
+++ routes/api.php
@@ -50,7 +50,7 @@ Route::get('/financials',function (){
             return Carbon::parse($val->financial_date)->format('Y');
         }),
         'reports'=>$year->groupBy(function($val) {
-            return Carbon::parse($val->financial_date)->format('Y');
+            return Carbon::parse($val->year_date)->format('Y');
         }),
         'financialsAndYear'=>\App\Models\Financial::select('financial_file',DB::raw('YEAR(financials.financial_date) AS Date'),DB::raw('financials.title'))->addSelect(
 
