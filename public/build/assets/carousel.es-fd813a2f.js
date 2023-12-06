d9a230fd9bf4560a3f6f160fe64ab6cf83a1f2ae
mohamedemadal <142974789+mohamedemadal@users.noreply.github.com>
2023-12-06 11:04:30 +0200

    new commit

--- public/build/assets/carousel.es-fd813a2f.js
+++ public/build/assets/carousel.es-fd813a2f.js
@@ -1,4 +1,4 @@
-import{C as te,z as m,g as u,y as p,I as L,H as y,D as be,K as xe,G as ye,B as Q,E as J,x as fe}from"./main-c30eb628.js";/**
+import{C as te,z as m,g as u,y as p,I as L,H as y,D as be,K as xe,G as ye,B as Q,E as J,x as fe}from"./main-3319f655.js";/**
  * Vue 3 Carousel 0.2.12
  * (c) 2023
  * @license MIT
