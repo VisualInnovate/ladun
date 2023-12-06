5cdd39f050e0e7fbe9dc26b8185a365daf0bf549
mohamedemadal <142974789+mohamedemadal@users.noreply.github.com>
2023-12-06 10:55:23 +0200

    new commit

--- app/Filament/Resources/FinancialResource.php
+++ app/Filament/Resources/FinancialResource.php
@@ -35,7 +35,7 @@ class FinancialResource extends Resource
             ->schema([
                 TextInput::make('title')->label(__('title')),
                 DatePicker::make('financial_date')->required(),
-                FileUpload::make('financial_file')->acceptedFileTypes(['application/pdf'])
+                FileUpload::make('financial_file')->acceptedFileTypes(['application/pdf'])->required()
                 ->getUploadedFileNameForStorageUsing(function (TemporaryUploadedFile $file): string {
                     return (string) str($file->getClientOriginalName())->prepend('custom-prefix-');
                 })
