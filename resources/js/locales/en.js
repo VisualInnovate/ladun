fbb79f9e6b5291e2517620915a9e7733fc379150
Magdy <129180170+MagdyGhazy@users.noreply.github.com>
2024-03-06 12:08:42 +0200

    .

--- resources/js/locales/en.js
+++ resources/js/locales/en.js
@@ -206,4 +206,106 @@ export default {
     Disclaimer: "Disclaimer",
 
 
+    Terms_Conditions: "Terms & Conditions",
+    Terms_Conditions_text: "Welcome to the Ladun Investment Company website (www.Ladun.sa and/or its sub-sites, \"the Site\").These Terms and Conditions of Use (“Terms of Use”) are made between Ladun Investment Company a company established under the laws of the Kingdom of Saudi Arabia, with its registered office address at Ladun Investment Company Sultan Center – Al Amir Sultan Ibn Abdul Aziz st., North Mathar Dist., Building No. 2751, Unit No. 1, Additional No. 6769, Zip Code 12312, Riyadh, Kingdom of Saudi Arabia; and You and govern your access to and use of the Site and the Platforms (as defined below). Ladun Investment Company offers the Site, including all information, tools, and services available from the Site, to You conditioned upon your acceptance of these Terms of Use and Our Privacy Policy. The services and the information obtained through this Site do not alter, modify or supersede the terms or conditions of any other agreement You may have with Ladun Investment Company or any of its subsidiaries or Affiliates. These Terms of Use (together, with any documents referred to herein) set out the terms under which You may access and use, whether as a guest or as a registered user:\n" +
+        "\n" +
+        "Our Site.\n" +
+        "Our mobile application.\n" +
+        "Our social media pages; and\n" +
+        "Our other technological platforms are in whatever form. (together referred to as \"Our Platforms\" or \"the Platforms\").\n" +
+        "This Terms of Use is a legal document setting forth rights and obligations between You and Ladun Investment Company. The Terms of Use are to be read in conjunction with Our Privacy Policy, which sets out the terms on which We process any personal data We collect from You, or that You provide to Us. Please read the Terms of Use and Our Privacy Policy carefully before You start using Our Platforms, as these documents will apply to your use of, including accessing, browsing or registering to use, Our Platforms. By using Our Platforms, You confirm that You accept these Terms of Use and Our Privacy Policy and that You agree to comply with them. If You do not agree to these Terms of Use or Our Privacy Policy, You must not use Our Platforms or access the site.\n",
+
+
+    Age_Restriction: "Age Restriction",
+    Age_Restriction_text: "If You are under the age of 18 years, You may only use Our Platforms under the supervision of a person that exercises parental or guardianship authority over You (i.e. a parent or guardian).",
+
+
+    Intellectual_Property_Rights: "Intellectual Property Rights",
+    Intellectual_Property_Rights_text: "In these Terms of Use, \"Intellectual Property Rights\" means all intellectual property rights and includes: Copyright, patents, database rights and rights in trademarks, designs, know-how and confidential information (whether registered or unregistered); Applications for registration, and the right to apply for registration, for any of these rights; All other intellectual property rights and equivalent or similar forms of protection existing anywhere in the world; Any licenses, permissions, or rights to use obtained by a third-party owner of intellectual property to use the Content on Our Platforms; \"Content\" means Our Platforms, including but not limited to the underlying HTML, text, design, graphics, logos, button icons and images, audio clips, video clips, digital downloads (as well as the organization and layout of Our Platforms), press releases, software and other content, including that which is provided by external Third-Party Service providers, published and made available to You on Our Platforms.\n" +
+        "\n" +
+        "Ladun Investment Company owns (and where applicable is the licensee of) all Intellectual Property Rights in the Content. All rights, including those not expressly granted, in and to the Platforms and the Content accessible thereon, are reserved by Ladun Investment Company.\n" +
+        "\n" +
+        "Except as otherwise described herein, nothing on Our Platforms shall be construed as granting, by implication, estoppel, or otherwise, any license or right to use any trademark, logo displayed on the Platforms without the owner's prior written permission. Our trademarks, logos, images and service marks used on this Site are Our property and may not be used without permission from Us and then only with proper acknowledgement.\n",
+
+    Use_of_Contents_on_the_Platforms: "Use of Contents on the Platforms",
+    Use_of_Contents_on_the_Platforms_text: "Limited license: You may access, copy, download and print the Content contained on Our Platforms for your personal and non-commercial use, provided You do not modify or delete any copyright, trademark or other proprietary notice that appears on the Content You access, copy, download or print. Any other use of Content on Our Platforms, including but not limited to the modification, alteration, creation of derivative works, distribution, transmission, performance, broadcast, publication, uploading, storage for subsequent use, licensing or sublicensing in whole or in part in any manner without Our prior written consent is expressly prohibited. You agree to abide by all additional restrictions displayed on Our Platforms as may be updated from time to time.\n" +
+        "\n" +
+        "The limited license that is referred to above is revocable at any time in Ladun Investment Company’s sole discretion. Ladun Investment Company neither warrants nor represents that your use of Content on the Platforms will not infringe rights of third parties, not affiliated with Ladun Investment Company.\n" +
+        "\n" +
+        "You may not use information provided on Our Platforms for any unauthorized, illegal or improper purposes, including without limitation marketing, the sending of unsolicited, harassing or slanderous messages, soliciting or promoting business or activities that are unlawful or any other activity that threatens the integrity or performance of any other person’s or entity’s computer system or violates generally accepted standards of internet usage. You may not use any hardware or software intended to damage or interfere with the proper working of the Platforms or to surreptitiously intercept any system, data or personal information from the Platforms. You agree not to interrupt or attempt to interrupt the operation of the site in any way. You are responsible for ensuring that your use of software, information and images obtained through your access of the Site complies with all applicable laws.\n" +
+        "\n" +
+        "By using Our Platforms, You warrant that You own any data, information or materials (“Materials”) You provide to Us. You warrant and represent that you are entitled to post comments and Materials on our Site and You have all necessary licenses and consents to do so. Any comments and Materials will not contain any defamatory, libelous, offensive, indecent or otherwise unlawful content which is an invasion of privacy. We discourage You from sending or posting to the Site any information that You consider proprietary or confidential. Any information you do sent or post, We will assume that it is not confidential and does not violate any intellectual property rights of a third party. You hereby grant to Us an unrestricted, irrevocable license to use, reproduce, display, perform, modify, transmit and distribute Materials and You agree that We are free to use any ideas, concepts, know-how or techniques that You send or post for any purpose.\n",
+
+
+
+    Our_Site_I_Not_an_Offer_to_Sell_Securities: "Our Site Is Not an Offer to Sell Securities",
+    Our_Site_I_Not_an_Offer_to_Sell_Securities_text: "The material contained in the Site is for informational purposes only. Nothing in the Site is intended as an inducement or invitation for any person to acquire securities in any jurisdiction. Nothing in the Site constitutes or forms a part of any offer for sale or subscription of, or any invitation to offer to buy or subscribe for, any securities, nor should it or any part of it form the basis of, or be relied upon in any connection with any contract or commitment whatsoever. The material is not to be construed as an offer or a recommendation to buy or sell a security.",
+
+
+
+    Accessing_Our_Platforms: "Accessing Our Platforms",
+    Accessing_Our_Platforms_text: "We do not guarantee that Our Platforms, or any Content on them, will always be available or be uninterrupted. We may suspend, withdraw, discontinue or change all or any part of Our Platforms without notice. We will not be liable to You, if for any reason, Our Platforms are unavailable at any time or for any period. Ladun Investment Company reserves the right to refuse or cancel any person's registration to Our Platforms, or remove any person or prohibit any person from using Our Platforms, at its sole discretion, at any time and without notice. Termination of your access or use will not waive or affect any other right or relief, to which Ladun Investment Company may be entitled to by Law.",
+
+
+    Third_Party_Services: "Third-Party Services",
+    Third_Party_Services_text: "Certain links on Our Platform lead to content and resources that are located on websites and servers not under the control of Ladun Investment Company, or for which organizations other than Ladun Investment Company are responsible. Such websites, resources and any products and services offered through them are referred to as \"Third-Party Services\". Any links to Third-Party Services are provided as a convenience to You only. Ladun Investment Company takes no responsibility for the accuracy, content or any aspect of any Third-Party Service, whether linked to Our Platforms or not, and disclaims any and all liability to You therefor or for any consequence of your decision to use or your use of any such Third-Party Service. You understand and agree that:\n" +
+        "\n" +
+        "Your use of any Third-Party Service through Our Platforms is subject to such terms, conditions, and limitations that the Third-Party Service may impose as a condition of such use, and You agree to abide by all such restrictions.\n" +
+        "Either Ladun Investment Company or a provider of a Third-Party Service may restrict or terminate your access to such Third-Party Service at any time; and\n" +
+        "You shall not bring a suit or claim against Ladun Investment Company, its partners, employees, agents, contractors, suppliers, or any of them arising from or based on your purchase or use of resources, products, or services through any Third-Party Service.\n",
+
+
+
+    Changes_and_Updates_to_These_Terms_of_Use: "Accessing Our Platforms",
+    Changes_and_Updates_to_These_Terms_of_Use_text: "From time to time, we may update, revise, or change these Terms of Use and/or Our Privacy Policy. These changes will be posted on Our Site. Please regularly check these pages for the latest version of these Terms of Use and Our Privacy Policy as it is your responsibility to ensure compliance. Third-Party Service providers may also change their terms of use from time to time. Ladun Investment Company may not always update these terms to reflect these changes immediately and cannot guarantee that the hyperlinks to the third-party websites are valid and usable.",
+
+
+
+
+    Changes_and_Updates_to_Our_Platforms: "Changes and Updates to Our Platforms",
+    Changes_and_Updates_to_Our_Platforms_text: "We may update Our Platforms from time to time and may change the Content at any time. Please note that any of the Content on Our Platforms may be out of date at any given time, and We are under no obligation to update it. We do not guarantee that Our Platforms, or any Content on them, will be free from errors, omissions, or defects, whether material or otherwise, and assume no responsibility or liability in relation thereto.",
+
+
+
+    Viruses: "Viruses",
+    Viruses_text: "We do not warrant or guarantee that Our Platforms will be secure or free from bugs or viruses. You are responsible for configuring your information technology, computer programmers and platform in order to access Our Platforms. You should use your own virus protection software.\n" +
+        "\n" +
+        "You must not misuse Our Platforms by knowingly introducing viruses, trojans, worms, logic bombs or other material which is malicious or technologically harmful. You must not attempt to gain unauthorized access to Our Platforms, the server on which Our Platforms are stored or any server, and computer or database connected to Our Platforms. By breaching this provision, you would commit under the laws of the Kingdom of Saudi Arabia and other countries. We will report any such breach to the relevant law enforcement authorities, and We will cooperate with those authorities by disclosing your identity to them. In the event of such a breach, your right to use Our Platforms will cease, immediately.\n" +
+        "\n" +
+        "We do not warrant or guarantee that Our Platforms will be secure, including from cyber-attacks, which may either seek to disrupt access to Our Platforms or steal data contained on Our Platforms. You access Our Platforms and submit any data to the Platforms entirely at your own risk.\n",
+
+
+
+
+    Exclusion_of_Liabilities: "Exclusion of Liabilities",
+    Exclusion_of_Liabilities_text: "To the fullest extent permissible by the Laws of the Kingdom of Saudi Arabia, Ladun Investment Company excludes its liability arising from your access to and use of Our Platforms. We assume no responsibility for and make no representations with respect to the accuracy of any information presented on the Site. Therefore, all materials and information is presented AS IS and WE EXPRESSLY DISCLAIM ANY IMPLIED OR EXPRESS WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, OR NON-INFRINGEMENT OF INTELLECTUAL PROPERTY RELATING TO SUCH MATERIAL. IN NO EVENT SHALL WE BE LIABLE TO YOU OR TO ANY OTHER PERSON, WHETHER IN CONTRACT, TORT OR OTHERWISE, FOR ANY LOSS, LIABILITY, COST, DAMAGE OR OTHER INJURY OF ANY KIND WHATSOEVER, INCLUDING FOR CONSEQUENTIAL, INCIDENTAL, INDIRECT, SPECIAL, OR PUNITIVE DAMAGES, INCLUDING LOST PROFITS, BUSINESS INTERRUPTION, LOSS OF PROGRAMS OR DATA ON ANY INFORMATION HANDLING SYSTEMS OR OTHERWISE, ARISING OUT OF OR RESULTING FROM:\n" +
+        "\n" +
+        "Any access to or use of the Platforms or Content, or due to any connectivity issues occurring at your premises, even if expressly advised of the possibility thereof;\n" +
+        "Any inability by You to access the Platforms or Content for any reason whatsoever;\n" +
+        "Any attack upon the Platforms or Content, including any theft or loss of data from the Platforms or Content;\n" +
+        "Any virus, distributed denial of service attack, or other technologically harmful material that may infect your computer equipment, computer programs, data or other proprietary material due to your use of the Platforms or the Content or to your downloading of any Content on it, or on any application, website or content linked to it.\n" +
+        "Without prejudice or limitation to any of the foregoing, to the fullest extent permissible by applicable laws, Ladun Investment Company excludes any liability on Our behalf and on behalf of any Our Affiliates for any claims or matters arising under or in connection with these Terms of Use. You may not make a claim or bring proceedings relating to the Platforms or Content or against any other Ladun Investment Company Affiliate to the extent that no direct contractual relationship exists between You and those entities. You shall make any claim or bring proceedings only against Us on the basis that We are the party with whom You have contracted under these Terms of Use.\n" +
+        "\n" +
+        "You agree to protect and defend Us against all claims arising from the Materials that You provide to Us. We shall not be held liable for any content that appears on your website.\n" +
+        "\n" +
+        "For the purposes of these Terms of Use, “Affiliate” means an entity or individual that controls, is controlled by, or is under common control with, the first entity, and \"Control\" means the ability to direct the policies or operations of an entity, whether by contract, ownership of equity interests, or otherwise.\n" +
+        "\n" +
+        "Any claim made in relation to the Platform or Content or otherwise under these Terms of Use, must be brought no later than twelve (12) months from the date of the act or omission alleged to have caused the claim.\n" +
+        "\n" +
+        "You must not misuse Our Platforms by knowingly introducing viruses, trojans, worms, logic bombs or other material which is malicious or technologically harmful. You must not attempt to gain unauthorized access to Our Platforms, the server on which Our Platforms are stored or any server, and computer or database connected to Our Platforms. By breaching this provision, You would commit under the laws of the Kingdom of Saudi Arabia and other countries. We will report any such breach to the relevant law enforcement authorities, and We will cooperate with those authorities by disclosing your identity to them. In the event of such a breach, your right to use Our Platforms will cease, immediately.\n" +
+        "\n" +
+        "We do not warrant or guarantee that Our Platforms will be secure, including from cyber-attacks, which may either seek to disrupt access to Our Platforms or steal data contained on Our Platforms. You access Our Platforms and submit any data to the Platforms entirely at your own risk.\n",
+
+
+
+
+    Applicable_Law: "Applicable Law",
+    Applicable_Law_text: "The Laws of the Kingdom of Saudi Arabia will govern these Terms of Use and the competent courts of the Kingdom of Saudi Arabia shall have exclusive jurisdiction over any disputes arising out of these.",
+
+
+    Entire_agreement: "Entire agreement",
+    Entire_agreement_text: "These Terms of Use constitute the entire agreement between You and Ladun Investment Company for the use of this Site and shall take precedence over any disclaimer and/or notice attached to any communication and/or posting received from you.",
+
+
+
 };
