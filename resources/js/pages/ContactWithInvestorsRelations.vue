f7397597e80ab7c891be045ffd36c95e230d56fa
Mohamed-Front <malshiekh41@gmail.com>
2024-03-09 22:50:07 -0800

    new commit

--- resources/js/pages/ContactWithInvestorsRelations.vue
+++ resources/js/pages/ContactWithInvestorsRelations.vue
@@ -28,7 +28,7 @@
                     <div class="flex flex-col dark:text-white dark:bg-black" >
                         <div class="flex flex-col md:flex md:flex-row my-auto gap-x-2 dark:text-white dark:bg-black">
                             <p class="font-bold inline dark:text-white dark:bg-black">{{ $t('phone') }}</p> <a class="underline" target="_blank" href="https://wa.me/920011560">: {{settings[0].phone}} </a>
-                            <p class="font-bold inline dark:text-white dark:bg-black">{{ $t('ext') }}</p><a class="underline" :href="settings[0].convert" target="_blank"> : {{settings[0].convert}}</a>
+                            <!-- <p class="font-bold inline dark:text-white dark:bg-black">{{ $t('ext') }}</p><a class="underline" :href="settings[0].convert" target="_blank"> : {{settings[0].convert}}</a> -->
                         </div>
                         <div class="my-auto">
                             <p class="font-bold inline">{{ $t('fax') }}</p><a class="underline" :href="settings[0].fax" target="_blank"> : {{settings[0].fax}}</a>
