0a2eda85153b2ef83c5028cce33f635ca1c3974d
Mohamed-Front <malshiekh41@gmail.com>
2024-03-11 05:09:48 -0700

    new commit

--- resources/js/components/Modal.vue
+++ resources/js/components/Modal.vue
@@ -63,7 +63,7 @@
 
                      </select>
 
-
+                     <input  id="00N8d00000RMxGu" maxlength="255" name="00N8d00000RMxGu" size="20" type=hidden  />
                     <input type="submit" class="text-white bg-[#AF9744] hover:bg-[#764522] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" :value="$t('submit')">
                 </form>
 
