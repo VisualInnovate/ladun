626e89656b78cf11985a3b484ee3436d73e698aa
hassan-aamer <hassanaamer048@gmail.com>
2025-11-05 18:03:42 +0200

    update mali

--- resources/views/emails/new-contact.blade.php
+++ resources/views/emails/new-contact.blade.php
@@ -8,8 +8,10 @@
     <h2>A new connection has been created</h2>
     <p><strong>First name:</strong> {{ $contact->first_name ?? '' }}</p>
     <p><strong>Last name:</strong> {{ $contact->last_name ?? '' }}</p>
-    <p><strong>Project name:</strong> {{ $contact->message ?? '' }}</p>
     <p><strong>Mail:</strong> {{ $contact->email ?? '' }}</p>
     <p><strong>Phone:</strong> {{ $contact->phone ?? '' }}</p>
+    <p><strong>Project name:</strong> {{ $contact->message ?? '' }}</p>
+    <p><strong>Ownership:</strong> {{ $contact->ownership ?? '' }}</p>
+    <p><strong>Area:</strong> {{ $contact->area ?? '' }}</p>
 </body>
 </html>
