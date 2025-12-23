b6b8ba82e0213c0ae28d682007eb1542d6da05a4
Magdy <129180170+MagdyGhazy@users.noreply.github.com>
2025-12-23 12:47:10 +0200

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,48 +1,17 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-5z247LVX');</script>
+    <!-- End Google Tag Manager -->
+
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <!-- Static Google Tag Manager (added per request) -->
-    <script id="gtm-static-script" async src="https://www.googletagmanager.com/gtm.js?id=GTM-5Z247LVX"></script>
-    <script>
-        (function(){
-            try{
-                var s = document.getElementById('gtm-static-script');
-                if(s){
-                    s.addEventListener('load', function(){
-                        console.log('Static GTM loaded: GTM-5Z247LVX');
-                        try{
-                            var params = new URLSearchParams(window.location.search);
-                            if(params.get('gtm_debug')==='1' || localStorage.getItem('gtm_debug')==='1'){
-                                alert('Static GTM loaded: GTM-5Z247LVX');
-                            }
-                        }catch(e){}
-                    });
-                }
-            }catch(e){console.error(e)}
-        })();
-    </script>
-
-    <!-- Dynamic Google Tag Manager - Loaded from database settings -->
-    @if($page['props']['settings']['gtm_id'] ?? null)
-        <!-- Google Tag Manager -->
-        <script>
-            (function(w,d,s,l,i){
-                w[l]=w[l]||[];
-                w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
-                var f=d.getElementsByTagName(s)[0],
-                    j=d.createElement(s),
-                    dl=l!='dataLayer'?'&l='+l:'';
-                j.async=true;
-                j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-                f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','{{ $page['props']['settings']['gtm_id'] }}');
-        </script>
-        <!-- End Google Tag Manager -->
-    @endif
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
@@ -55,19 +24,10 @@
     @inertiaHead
 </head>
 <body class="font-sans antialiased">
-
-    <!-- Static Google Tag Manager (noscript fallback) -->
-    <noscript id="gtm-static-noscript">
-        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5Z247LVX" height="0" width="0" style="display:none;visibility:hidden"></iframe>
-    </noscript>
-
-    <!-- Google Tag Manager (noscript fallback for dynamic settings, if present) -->
-    @if($page['props']['settings']['gtm_id'] ?? null)
-        <noscript>
-            <iframe src="https://www.googletagmanager.com/ns.html?id={{ $page['props']['settings']['gtm_id'] }}"
-                    height="0" width="0" style="display:none;visibility:hidden"></iframe>
-        </noscript>
-    @endif
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5z247LVX"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
 
     <x-jet-banner />
 
